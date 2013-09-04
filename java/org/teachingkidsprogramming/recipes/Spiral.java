6f748d4817c3c1508b2afe7bc811f5e7573c4b60
Llewellyn Falco <isidore@setgame.com>
2013-09-04 12:48:41 -0700

    better order

--- java/org/teachingkidsprogramming/recipes/Spiral.java
+++ java/org/teachingkidsprogramming/recipes/Spiral.java
@@ -6,9 +6,9 @@ public class Spiral
   {
     //    Show the tortoise --#1
     //    Make the tortoise go as fast as possible --#4
-    //    Add Blue Violet to the Color Wheel --#7
+    //    Add Purple to the Color Wheel --#7
     //    Add Violet to the Color Wheel --#8
-    //    Add Purple to the Color Wheel --#9
+    //    Add Blue Violet to the Color Wheel --#9
     //    Do the following 75 times --#3
     //         Change the color of the line the tortoise draws the next color on the Color Wheel --#6
     //         Move the tortoise 5 times the current line number you are drawing --#5
