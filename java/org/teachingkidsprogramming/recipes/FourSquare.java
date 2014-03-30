5d61e1f711c962080704d167676f502fe55cdb6b
Llewellyn Falco <isidore@setgame.com>
2014-03-30 14:09:04 -0700

    line numbers

--- java/org/teachingkidsprogramming/recipes/FourSquare.java
+++ java/org/teachingkidsprogramming/recipes/FourSquare.java
@@ -5,18 +5,18 @@ public class FourSquare
   public static void main(String[] args)
   {
     //  Show the tortoise --#1
-    //  Make the tortoise move as fast as possible --#8
-    //  Do the following 4 times --#9
-    //      drawSquare (recipe below) --#7
+    //  Make the tortoise move as fast as possible --#7
+    //  Do the following 4 times --#8
+    //      drawSquare (recipe below) --#6
     //      ------------- Recipe for drawSquare --#6
     //      Do the following 4 times --#5
     //          Change the color of the line the tortoise draws to a random color --#3
     //          Move the tortoise 50 pixels --#2
     //          Turn the tortoise 90 degrees to the right --#4
-    //      Repeat
+    //      Repeat --#5
     //      ------------- End of drawSquare recipe --#6
-    //      Turn the tortoise 90 degrees to the right --#10
-    //  Repeat
+    //      Turn the tortoise 90 degrees to the right --#9
+    //  Repeat --#8
   }
   // Related Videos:
   // Sub recipe-> Method : http://youtu.be/C6fnqjceVcs
