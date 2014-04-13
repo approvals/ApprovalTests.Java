a3a366cb54b63ea098395c0537601e977ad77ffc
Lynn Langit <lynnlangit@live.com>
2014-04-13 13:18:52 -0700

    changed English and formatting for TKP Java
    updated quiz English from SmallBasic to TKP Java

--- java/org/teachingkidsprogramming/recipes/quizzes/SpiderWebQuiz.java
+++ java/org/teachingkidsprogramming/recipes/quizzes/SpiderWebQuiz.java
@@ -5,35 +5,41 @@ import org.teachingkidsprogramming.recipes.quizzes.graders.SpiderWebQuizGrader;
 
 public class SpiderWebQuiz extends SpiderQuiz
 {
-  public void question1()
-  {
-    //         Do the following the current number of times
-    //        Call circle()
-    //        Repeat
-  }
-  //
-  //      Question2
-  //       Create a subroutine called circleAround which 
-  //      Does the following 3 times
-  //      Call adjust()
-  //       Call question1
-  //      Repeat
-  //
-  //
-  //      Question3
-  //      Create a subroutine called grow which 
-  //       Changes the current length so it is multiplied by 2.5
-  //
-  //
-  //      Question4
-  //      Create a subroutine called shrink which 
-  //       Decreases the current length by 9 pixels
-  //
-  //
-  //      Question5
-  //      Create a subroutine called expand which
-  //      Increases the current number by 12
-  //
+  //       Question1
+public void question1()
+  {
+    //        Do the following the current number of times
+    //        Call circle
+    //        Repeat
+  }
+    //   Question2
+    //   circleAround (recipe below) 
+    //   ------------- Recipe for circleAround 
+    //      Does the following 3 times
+    //      Call adjust()
+    //      Call question1
+    //      Repeat
+    //   ------------- End of circleAround
+    //
+    //      Question3
+    //      grow (recipe below) 
+    //   ------------- Recipe for grow 
+    //       Set the current length so it is multiplied by 2.5
+    //   ------------- End of grow
+    //
+    //      Question4
+    //      shrink (recipe below) 
+    //   ------------- Recipe for shrink 
+    //       Decrease the current length by 9 pixels
+    //   ------------- End of shrink
+    //
+    //      Question5
+    //      expand (recipe below) 
+    //   ------------- Recipe for expand 
+    //       Increase the current number by 12
+    //   ------------- End of expand
+
+  
   public static void main(String[] args)
   {
     new SpiderWebQuizGrader().grade(new SpiderWebQuiz());
