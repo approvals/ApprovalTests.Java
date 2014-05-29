af80aa3f47341e04b65be23b54936b3175af6a75
Johan Martinsson <martinsson.johan@gmail.com>
2014-05-29 16:16:01 +0300

    fix initialization issue in winmergereporter

--- java/org/approvaltests/reporters/windows/WinMergeReporter.java
+++ java/org/approvaltests/reporters/windows/WinMergeReporter.java
@@ -4,10 +4,11 @@ import org.approvaltests.reporters.GenericDiffReporter;
 
 public class WinMergeReporter extends GenericDiffReporter
 {
+  public static String[] POSSIBLE_LOCATIONS = new String[]{ "C:\\Program Files\\WinMerge\\WinMergeU.exe",
+                                                            "C:\\Program Files (x86)\\WinMerge\\WinMergeU.exe",
+                                                            "C:\\Programs\\WinMerge\\WinMergeU.exe"};
   public static final WinMergeReporter INSTANCE     = new WinMergeReporter();                       ;
-  private static final String[] POSSIBLE_LOCATIONS = { "C:\\Program Files\\WinMerge\\WinMergeU.exe",
-                                                       "C:\\Program Files (x86)\\WinMerge\\WinMergeU.exe",
-                                                       "C:\\Programs\\WinMerge\\WinMergeU.exe"} ;
+  
   public WinMergeReporter()
   {
     super(POSSIBLE_LOCATIONS, GenericDiffReporter.WINDOWS_ARGUMENT_FORMAT);
