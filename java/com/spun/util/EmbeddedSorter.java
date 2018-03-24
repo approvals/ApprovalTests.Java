89876352f25978156bea41e3faa316231355b48e
Llewellyn Falco <isidore@setgame.com>
2018-03-24 20:46:03 +0200

    Depercated EmbeddedSorter

--- java/com/spun/util/EmbeddedSorter.java
+++ java/com/spun/util/EmbeddedSorter.java
@@ -1,26 +1,9 @@
 package com.spun.util;
 
-import java.util.Comparator;
-
-public class EmbeddedSorter <T>
-  implements java.util.Comparator<T>
+/**
+ * @deprecated use Lambdas (a,b) -> comparator.compare(a.method(),b.method())
+ */
+@Deprecated
+public class EmbeddedSorter<T>
 {
-  private MethodExecutionPath executionPath;
-  private Comparator comparator;
-  
-  /************************************************************************/
-  public EmbeddedSorter(Comparator comparator, MethodExecutionPath executionPath)
-  {
-    this.comparator = comparator;
-    this.executionPath = executionPath;
-  }
-  /************************************************************************/
-  public int compare(T o1, T o2)
-  {
-    Object e1 = executionPath.extractValue(o1);
-    Object e2 = executionPath.extractValue(o2);
-    return comparator.compare(e1, e2);
-  }
-  /************************************************************************/
-  /************************************************************************/
 }
