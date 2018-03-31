3f3aec60533d29c4a807ad863591c307af9ee4a0
Llewellyn Falco <isidore@setgame.com>
2018-03-31 22:48:54 +0300

    Removed package com.spun.util.shipping;

--- java/com/spun/util/database/automaticsetter/ChangeDateAware.java
+++ java/com/spun/util/database/automaticsetter/ChangeDateAware.java
@@ -2,13 +2,8 @@ package com.spun.util.database.automaticsetter;
 
 import java.sql.Timestamp;
 
-
 public interface ChangeDateAware
 {
-  /***********************************************************************/
   public boolean setChangeDate(Timestamp time);
-  /***********************************************************************/
   public Timestamp getChangeDate();
-  /***********************************************************************/
-  /***********************************************************************/
 }
