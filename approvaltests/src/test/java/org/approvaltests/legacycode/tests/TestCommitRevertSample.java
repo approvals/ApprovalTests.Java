b02d346be13beb5cda6a6b23ce064afac888e5d5
Llewellyn Falco <isidore@setgame.com>
2019-04-22 18:06:18 -0700

    "t added snippets"

--- approvaltests/src/test/java/org/approvaltests/legacycode/tests/TestCommitRevertSample.java
+++ approvaltests/src/test/java/org/approvaltests/legacycode/tests/TestCommitRevertSample.java
@@ -5,7 +5,9 @@ import static org.junit.Assert.assertEquals;
 import org.approvaltests.legacycode.TestCommitRevert;
 import org.junit.Test;
 
+//startcode test_commit_revert 
 public class TestCommitRevertSample extends TestCommitRevert
+// endcode
 {
   @Test
   public void testOnSuccessThisPromptsForCommitMessage()
