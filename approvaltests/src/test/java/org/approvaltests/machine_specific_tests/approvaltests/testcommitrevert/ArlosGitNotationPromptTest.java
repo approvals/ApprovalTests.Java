fc45b39d5fd63065e7d6988c3b6bf03783350969
Llewellyn Falco <isidore@setgame.com>
2020-06-09 16:37:31 -0700

    t test passing and moved under approvaltests subfolder

--- approvaltests/src/test/java/org/approvaltests/machine_specific_tests/approvaltests/testcommitrevert/ArlosGitNotationPromptTest.java
+++ approvaltests/src/test/java/org/approvaltests/machine_specific_tests/approvaltests/testcommitrevert/ArlosGitNotationPromptTest.java
@@ -1,7 +1,7 @@
-package machine_specific_tests.approvaltests.testcommitrevert;
+package org.approvaltests.machine_specific_tests.approvaltests.testcommitrevert;
 
-import machine_specific_tests.MachineSpecificTest;
 import org.approvaltests.awt.AwtApprovals;
+import org.approvaltests.machine_specific_tests.MachineSpecificTest;
 import org.approvaltests.testcommitrevert.ArlosGitNotationPrompt;
 import org.junit.Test;
 
