dd5331c97b272036de5c2ef5dae4172ed36ca357
Llewellyn Falco <isidore@setgame.com>
2020-06-09 16:39:26 -0700

    t test passing and moved under approvaltests subfolder

--- html_locker/src/test/java/machine_specific_tests/approvaltests/webpages/tests/WebPageTest.java
+++ html_locker/src/test/java/machine_specific_tests/approvaltests/webpages/tests/WebPageTest.java
@@ -3,14 +3,13 @@ package machine_specific_tests.approvaltests.webpages.tests;
 import java.net.URI;
 
 import org.approvaltests.awt.AwtApprovals;
+import org.approvaltests.machine_specific_tests.MachineSpecificTest;
 import org.approvaltests.reporters.ClipboardReporter;
 import org.approvaltests.reporters.DiffReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.approvaltests.webpages.WebPageApproval;
 import org.approvaltests.webpages.WebPageChangeDetector;
 import org.junit.Test;
-
-import machine_specific_tests.MachineSpecificTest;
 
 @UseReporter({DiffReporter.class, ClipboardReporter.class})
 public class WebPageTest extends MachineSpecificTest
