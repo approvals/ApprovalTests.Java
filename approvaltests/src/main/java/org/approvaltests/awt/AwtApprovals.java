ec63ac06f38b6a3fde60426fe34af338cdced828
Lars Eckart <lars.eckart@gmail.com>
2020-06-12 15:57:50 +0300

    r optimize imports

--- approvaltests/src/main/java/org/approvaltests/awt/AwtApprovals.java
+++ approvaltests/src/main/java/org/approvaltests/awt/AwtApprovals.java
@@ -1,14 +1,14 @@
 package org.approvaltests.awt;
 
 import com.spun.util.images.ImageWriter;
+import java.awt.Component;
+import java.awt.Image;
 import org.approvaltests.Approvals;
-import org.approvaltests.namer.ApprovalNamer;
 import org.approvaltests.namer.NamedEnvironment;
 import org.approvaltests.namer.NamerFactory;
 import org.approvaltests.writers.ComponentApprovalWriter;
 import org.approvaltests.writers.ImageApprovalWriter;
 
-import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class AwtApprovals {
