aa56d2e15dcf327ca1065ce6b57b5a29755e4dd2
Lars Eckart <lars.eckart@gmail.com>
2020-06-13 01:19:33 +0300

    F remove servlets, add MockClock to util

--- approvaltests-util/src/main/java/com/spun/util/servlets/SecondaryErrorProcessor.java
+++
@@ -1,8 +0,0 @@
-package com.spun.util.servlets;
-
-import com.spun.util.parser.TemplateError;
-
-public interface SecondaryErrorProcessor
-{
-  public String processError(TemplateError error, Throwable t2);
-}
