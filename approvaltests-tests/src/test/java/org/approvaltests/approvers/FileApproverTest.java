aa9efb8bfef3fe52ba1cd5b9dba70cea6f6d76b8
Lars Eckart <lars.eckart@gmail.com>
2020-06-16 00:02:47 +0300

    t migrate tests to junit5
    Co-Authored-By: Steve Turley <steve@turleylabs.com>
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- approvaltests-tests/src/test/java/org/approvaltests/approvers/FileApproverTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/approvers/FileApproverTest.java
@@ -1,38 +1,43 @@
 package org.approvaltests.approvers;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.approvaltests.Approvals;
-import org.approvaltests.approvers.FileApprover;
 import org.approvaltests.namer.ApprovalNamer;
 import org.approvaltests.writers.ApprovalTextWriter;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.lambda.functions.Function2;
 
+import com.spun.util.ObjectUtils;
 import com.spun.util.io.FileUtils;
 import com.spun.util.tests.StackTraceReflectionResult;
 import com.spun.util.tests.TestUtils;
 
-import junit.framework.TestCase;
-
-public class FileApproverTest extends TestCase
+public class FileApproverTest
 {
-  public void testGetCurrentFileForMethod() throws Exception
+  @Test
+  public void testGetCurrentFileForMethod()
   {
     StackTraceReflectionResult info = TestUtils.getCurrentFileForMethod(0);
     assertEquals("FileApproverTest", info.getClassName());
     assertEquals("testGetCurrentFileForMethod", info.getMethodName());
   }
-  public void testApproveTextFile() throws Exception
+  @Test
+  public void testApproveTextFile()
   {
     File f1 = createFile("1");
     File f2 = createFile("2");
-    assertFalse("files are different", FileApprover.approveTextFile(f2, f1));
+    assertFalse(FileApprover.approveTextFile(f2, f1), "files are different");
     f2 = createFile("1");
-    assertTrue("files are the same", FileApprover.approveTextFile(f2, f1));
+    assertTrue(FileApprover.approveTextFile(f2, f1), "files are the same");
   }
-  public void testApproveTextFileWithNonExsitantFile() throws Exception
+  @Test
+  public void testApproveTextFileWithNonExsitantFile()
   {
     File f1 = createFile("1");
     File f2 = new File("no exist");
@@ -40,11 +45,18 @@ public class FileApproverTest extends TestCase
     assertFalse(f2.exists());
     assertFalse(FileApprover.approveTextFile(f2, f1));
   }
-  private File createFile(String string) throws IOException
+  private File createFile(String string)
   {
-    File f = File.createTempFile("avc", "t");
-    FileUtils.writeFile(f, string);
-    return f;
+    try
+    {
+      File f = File.createTempFile("avc", "t");
+      FileUtils.writeFile(f, string);
+      return f;
+    }
+    catch (IOException e)
+    {
+      throw ObjectUtils.throwAsError(e);
+    }
   }
   @Test
   public void testCustomApprover()
