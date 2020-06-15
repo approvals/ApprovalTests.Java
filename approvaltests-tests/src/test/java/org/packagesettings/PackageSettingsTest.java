88d805ef8f6bb028d6cafe92faf9137391fd9f86
Lars Eckart <lars.eckart@gmail.com>
2020-06-16 00:35:56 +0300

    t migrate to junit 5
    Co-Authored-By: Steve Turley <steve@turleylabs.com>
    Co-Authored-By: Llewellyn Falco <isidore@users.noreply.github.com>

--- approvaltests-tests/src/test/java/org/packagesettings/PackageSettingsTest.java
+++ approvaltests-tests/src/test/java/org/packagesettings/PackageSettingsTest.java
@@ -3,12 +3,12 @@ package org.packagesettings;
 import java.util.Map;
 
 import org.approvaltests.Approvals;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class PackageSettingsTest
 {
   @Test
-  public void testRetriveValue() throws Exception
+  public void testRetriveValue()
   {
     // begin-snippet: package_level_settings_get
     Map<String, Settings> settings = PackageLevelSettings.get();
--- approvaltests-tests/src/test/java/org/packagesettings/subpackage/PackageSettingsTest.java
+++ approvaltests-tests/src/test/java/org/packagesettings/subpackage/PackageSettingsTest.java
@@ -1,23 +1,14 @@
 package org.packagesettings.subpackage;
 
-import static org.junit.Assert.fail;
-
 import org.approvaltests.Approvals;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.packagesettings.PackageLevelSettings;
 
 public class PackageSettingsTest
 {
   @Test
-  public void testRetriveValueWithOverRide() throws Exception
+  public void testRetriveValueWithOverRide()
   {
     Approvals.verify(PackageLevelSettings.get());
   }
-  /*
-  @Test
-  public void testName() throws Exception
-  {
-    fail("boo");
-  }
-   */
 }
