bd7b1d8b86e1027989957d5fadc8b6d165851afb
Llewellyn Falco <isidore@setgame.com>
2020-06-15 18:30:41 -0700

    R commons-net

--- approvaltests-util/src/main/java/com/spun/util/io/InternetConnectivityException.java
+++ approvaltests-util/src/main/java/com/spun/util/io/InternetConnectivityException.java
@@ -2,7 +2,7 @@ package com.spun.util.io;
 
 import java.net.InetAddress;
 
-import org.apache.commons.net.EchoTCPClient;
+import org.apache.commons.net.echo.EchoTCPClient;
 
 import com.spun.util.logger.SimpleLogger;
 
@@ -14,42 +14,36 @@ public class InternetConnectivityException extends Error
   private static final long serialVersionUID = 1L;
   private String            htmlText;
   private Throwable         cause;
-  
   public void setCause(Throwable t)
   {
     this.cause = t;
   }
-  
   public InternetConnectivityException(String string)
   {
     super(string);
   }
-  
   public void setHTMLText(String htmlText)
   {
     this.htmlText = htmlText;
   }
-  
   public String getHTMLText()
   {
     return htmlText;
   }
-  
   public static InternetConnectivityException testInternetConnectivity()
   {
     String[] sites = {"www.google.com", "www.yahoo.com", "www.msn.com"};
     for (int i = 0; i < sites.length; i++)
     {
-      if (pingSite(sites[i])) { return null; }
+      if (pingSite(sites[i]))
+      { return null; }
     }
     return new InternetConnectivityException("There is no internet connection.");
   }
-  
   public String toString()
   {
     return super.toString() + "\n" + (cause == null ? "" : "Causation:  " + cause.toString());
   }
-  
   private static boolean pingSite(String site)
   {
     try
@@ -66,6 +60,4 @@ public class InternetConnectivityException extends Error
       return false;
     }
   }
-  
-
 }
