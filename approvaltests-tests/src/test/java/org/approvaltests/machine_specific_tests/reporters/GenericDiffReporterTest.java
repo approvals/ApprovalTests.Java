ad11df1af1f3ca0e8849224de0a02e9d9d89c2d4
Llewellyn Falco <isidore@setgame.com>
2020-06-15 18:04:25 -0700

    t fixed machine specific test

--- approvaltests-tests/src/test/java/org/approvaltests/machine_specific_tests/reporters/GenericDiffReporterTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/machine_specific_tests/reporters/GenericDiffReporterTest.java
@@ -8,8 +8,6 @@ import org.approvaltests.namer.NamedEnvironment;
 import org.approvaltests.namer.NamerFactory;
 import org.approvaltests.reporters.GenericDiffReporter;
 import org.approvaltests.reporters.QueryableDiffReporterHarness;
-import org.approvaltests.reporters.UseReporter;
-import org.approvaltests.reporters.macosx.DiffMergeReporter;
 import org.approvaltests.reporters.macosx.MacDiffReporter;
 import org.approvaltests.reporters.windows.TortoiseTextDiffReporter;
 import org.approvaltests.reporters.windows.WinMergeReporter;
@@ -20,9 +18,9 @@ import com.spun.util.ClassUtils;
 
 public class GenericDiffReporterTest extends MachineSpecificTest
 {
-  @UseReporter(DiffMergeReporter.class)
   @Test
-  public void testGetWorkingReportersForEnvironment() {
+  public void testGetWorkingReportersForEnvironment()
+  {
     try (NamedEnvironment ne = NamerFactory.asMachineNameSpecificTest())
     {
       Approvals.verifyAll("reporters", MacDiffReporter.INSTANCE.getWorkingReportersForEnviroment());
@@ -30,15 +28,18 @@ public class GenericDiffReporterTest extends MachineSpecificTest
   }
   @Disabled("requires windows and TortoiseDiff installed")
   @Test
-  public void testTortoiseDiff() {
+  public void testTortoiseDiff()
+  {
     approveGenericReporter("a.txt", "b.txt", new TortoiseTextDiffReporter());
   }
   @Disabled("requires windows and WinMerge installed")
   @Test
-  public void testWinMerge() {
+  public void testWinMerge()
+  {
     approveGenericReporter("a.txt", "b.txt", new WinMergeReporter());
   }
-  private void approveGenericReporter(String a, String b, GenericDiffReporter reporter) {
+  private void approveGenericReporter(String a, String b, GenericDiffReporter reporter)
+  {
     File directory = ClassUtils.getSourceDirectory(getClass());
     String aPath = directory.getAbsolutePath() + File.separator + a;
     String bPath = directory.getAbsolutePath() + File.separator + b;
