a9c5ca3088b337fbb58ac78a78c4814e92329fc5
Llewellyn Falco <isidore@setgame.com>
2020-06-15 23:31:16 -0700

    e scripts

--- build/scripts/prepare_release.py
+++ build/scripts/prepare_release.py
@@ -9,6 +9,7 @@ from scripts.documentation_release import PrepareDocumentationRelease
 from scripts.release_constants import release_constants
 from scripts.release_details import ReleaseDetails
 from scripts.starter_project_release import PrepareStarterProjectRelease
+from scripts.utilities import run
 from scripts.version import Version
 
 
@@ -16,12 +17,22 @@ class PrepareRelease:
     def __init__(self, details: ReleaseDetails) -> None:
         self.details = details
 
+def publish_to_maven(details: ReleaseDetails) -> None:
+    new = details.new_version.get_version_text_without_v()
+    run(F"mvn versions:set -DnewVersion={new}")
+    # run("./publish_maven.sh")
+
+def set_snapshot(details: ReleaseDetails) -> None:
+    next = details.new_version.update_patch().get_version_text_without_v()
+    run(F"mvn versions:set -DnewVersion={next}-SNAPSHOT")
 
 def build(update_version: Callable[[Version], Version]) -> None:
     old_version = load_current_version()
     new_version = update_version(old_version)
     release_details = ReleaseDetails(old_version, new_version)
 
+    publish_to_maven(release_details);  
+    set_snapshot(release_details);
     PrepareDocumentationRelease.prepare_documentation(release_details)
     PrepareStarterProjectRelease.prepare_starter_project(release_details)
 
