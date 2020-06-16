9d02f8aebb2ced4c44a38883cb663ddcdffd6a54
Llewellyn Falco <isidore@setgame.com>
2020-06-15 23:11:41 -0700

    e cleaned scripts

--- build/deploy_major_release.py
+++ build/deploy_major_release.py
@@ -4,4 +4,4 @@ from scripts import version
 from scripts.prepare_release import build
 
 if __name__ == '__main__':
-    build(lambda v: v.update_major(), deploy = True)
+    build(lambda v: v.update_major())
--- build/deploy_patch_release.py
+++ build/deploy_patch_release.py
@@ -4,4 +4,4 @@ from scripts import version
 from scripts.prepare_release import build
 
 if __name__ == '__main__':
-    build(lambda v: v.update_patch(), deploy = True)
+    build(lambda v: v.update_patch())
