00dd50257d331cb37718f438a6a73e5ad3be8726
Lars Eckart <lars.eckart@gmail.com>
2020-06-23 01:13:18 +0300

    r deleted dead code + update todo list for next session

--- approvaltests-util/src/main/java/com/spun/util/constants/SpunConstants.java
+++
@@ -1,8 +0,0 @@
-package com.spun.util.constants;
-
-public class SpunConstants
-{
-
-  public static final String WORKSPACE = "C:\\Documents and Settings\\Llewellyn\\workspace\\";
-  public static final String SPUN_WORKSPACE = WORKSPACE + "Spun\\";
-}
