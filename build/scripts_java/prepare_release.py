9ab149eb7979c542b8828022a2c80903433c5096
Llewellyn Falco <isidore@setgame.com>
2020-06-29 13:56:43 -0700

    e cleanup scripts
    Co-Authored-By: Lars Eckart <larseckart@users.noreply.github.com>

--- build/scripts_java/prepare_release.py
+++ build/scripts_java/prepare_release.py
@@ -1,14 +1,16 @@
 import os
 from typing import Callable
 
-from scripts.utilities import run, check_step, assert_step
+from git import Repo
+
+from scripts.git_utilities import GitUtilities
+from scripts.utilities import run, assert_step
 from scripts.version import Version
 from scripts_java.documentation_release import PrepareDocumentationRelease
-from scripts.git_utilities import GitUtilities
 from scripts_java.release_constants import release_constants
 from scripts_java.release_details import ReleaseDetails
 from scripts_java.starter_project_release import PrepareStarterProjectRelease
-from git import Repo
+
 
 class PrepareRelease:
     def __init__(self, details: ReleaseDetails) -> None:
@@ -22,15 +24,15 @@ def publish_to_maven(details: ReleaseDetails) -> None:
 
 
 def set_snapshot(details: ReleaseDetails) -> None:
-    next = details.new_version.update_patch().get_version_text_without_v()
-    run(["mvn", "versions:set", f"-DnewVersion={next}-SNAPSHOT"])
+    next_version = details.new_version.update_patch().get_version_text_without_v()
+    run(["mvn", "versions:set", f"-DnewVersion={next_version}-SNAPSHOT"])
 
 
-def check_repo(release_details: ReleaseDetails) -> None:
+def check_repo() -> None:
     # check we are on a master branch that is in sync
     repo = Repo(".")
     assert_step((repo.active_branch.name == 'master'))
-    GitUtilities.check_no_uncommitted_changes(repo);
+    GitUtilities.check_no_uncommitted_changes(repo)
     assert_step(len(
         list(repo.iter_commits('master@{u}..master'))) == 0,
                 f"there are un-pushed changes in approvaltests")
@@ -42,15 +44,16 @@ def build(update_version: Callable[[Version], Version]) -> None:
     new_version = update_version(old_version)
     release_details = ReleaseDetails(old_version, new_version)
 
-    check_repo(release_details)
+    check_repo()
     publish_to_maven(release_details)
     PrepareDocumentationRelease.prepare_documentation(release_details)
     new_version.write("./build")
-    GitUtilities.add_and_commit_everything(".", new_version.get_version_text());
+    GitUtilities.add_and_commit_everything(".", new_version.get_version_text())
     set_snapshot(release_details)
-    GitUtilities.add_and_commit_everything(".", "set Snapshot");
+    GitUtilities.add_and_commit_everything(".", "set Snapshot")
+    GitUtilities.push_active_branch_origin(".")
     PrepareStarterProjectRelease.prepare_starter_project(release_details)
-    GitUtilities.push_active_branch_origin();
+
     print("Done")
 
 
