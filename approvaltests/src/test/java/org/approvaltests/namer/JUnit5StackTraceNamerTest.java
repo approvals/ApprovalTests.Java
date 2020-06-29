e594f9580b332b2faa6798027190f7c22b6b11b3
Lars Eckart <lars.eckart@gmail.com>
2020-06-30 00:26:27 +0300

    F supported JUnit5 RepeatedTest

--- approvaltests/src/test/java/org/approvaltests/namer/JUnit5StackTraceNamerTest.java
+++ approvaltests/src/test/java/org/approvaltests/namer/JUnit5StackTraceNamerTest.java
@@ -1,12 +1,18 @@
 package org.approvaltests.namer;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.RepeatedTest;
+import org.junit.jupiter.api.RepetitionInfo;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 public class JUnit5StackTraceNamerTest
 {
-
   @Test
   public void testGetApprovalName()
   {
@@ -27,5 +33,16 @@ public class JUnit5StackTraceNamerTest
       String className = JUnit5StackTraceNamerTest.class.getSimpleName() + "." + getClass().getSimpleName();
       StackTraceNamerUtils.assertApprovalName(className, "nestedTest");
     }
+  }
+  @Test
+  @DisplayName("hello")
+  public void testWithDisplayName()
+  {
+    StackTraceNamerUtils.assertNamerForFramework(getClass().getSimpleName(), "testWithDisplayName");
+  }
+  @RepeatedTest(2)
+  public void repeatedTest(RepetitionInfo repetitionInfo)
+  {
+    StackTraceNamerUtils.assertNamerForFramework(getClass().getSimpleName(), "repeatedTest");
   }
 }
