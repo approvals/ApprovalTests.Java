6739c836745d3dc23c4edea049ccdd1b1dc5e081
Llewellyn Falco <isidore@setgame.com>
2020-06-29 13:59:35 -0700

    e cleanup yellow
    Co-Authored-By: Lars Eckart <larseckart@users.noreply.github.com>

--- build/scripts_java/starter_project_release.py
+++ build/scripts_java/starter_project_release.py
@@ -1,5 +1,5 @@
 from scripts.git_utilities import GitUtilities
-from scripts.utilities import use_directory, replace_text_in_file, check_step
+from scripts.utilities import use_directory, replace_text_in_file
 from scripts_java.release_constants import release_constants
 from scripts_java.release_details import ReleaseDetails
 
@@ -7,11 +7,12 @@ from scripts_java.release_details import ReleaseDetails
 class PrepareStarterProjectRelease:
     @staticmethod
     def prepare_starter_project(details: ReleaseDetails) -> None:
-        GitUtilities.reset_and_clean_working_directory(release_constants.starter_project_dir)
+        project_dir = release_constants.starter_project_dir
+        GitUtilities.reset_and_clean_working_directory(project_dir)
         PrepareStarterProjectRelease.update_pom(details)
+        GitUtilities.add_and_commit_everything(project_dir, details.new_version.get_version_text())
+        GitUtilities.push_active_branch_origin(project_dir)
 
-        GitUtilities.add_and_commit_everything(release_constants.starter_project_dir, details.new_version.get_version_text())
-        GitUtilities.push_active_branch_origin(release_constants.starter_project_dir)
     @staticmethod
     def update_pom(details: ReleaseDetails) -> None:
         with use_directory(release_constants.starter_project_dir):
