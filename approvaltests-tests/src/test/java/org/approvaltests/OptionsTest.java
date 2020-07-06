8413986311ea1de3a74d64eaf40acd946ca063c1
Lars Eckart <lars.eckart@gmail.com>
2020-07-06 23:40:31 +0300

    t cleanup tests

--- approvaltests-tests/src/test/java/org/approvaltests/OptionsTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/OptionsTest.java
@@ -1,25 +1,21 @@
 package org.approvaltests;
 
-import com.spun.util.Asserts;
 import org.approvaltests.core.ApprovalFailureReporter;
 import org.approvaltests.core.Options;
-import org.approvaltests.reporters.FileLauncherReporter;
+import org.approvaltests.reporters.FirstWorkingReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.approvaltests.reporters.UseReporterTest;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.lambda.query.Query;
+
+import java.lang.reflect.Method;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class OptionsTest {
-
-    // Call without passing an option
-    // Call with option as last parameter
-    // Pass options constructed with a Reporter
-    // Pass options constructed with a builder pattern
-
-    // crate TestReporter
-    // verify with options of that getReporter
-    // will fail
-    // catch failure and
-    // check that getReporter was called
 
     @Test
     void verifyWithReporterInOptions() {
@@ -28,8 +24,7 @@ public class OptionsTest {
         try {
             Approvals.verify("hallo", options);
         } catch (Error error) {
-            // check that getReporter was called
-            Asserts.assertTrue("the getReporter should've been called", reporter.hasBeenCalled());
+            assertTrue(reporter.hasBeenCalled());
         }
     }
 
@@ -40,24 +35,27 @@ public class OptionsTest {
         try {
             Approvals.verify("hallo", options);
         } catch (Error error) {
-            // check that getReporter was called
-            Asserts.assertTrue("the getReporter should've been called", reporter.hasBeenCalled());
+            assertTrue(reporter.hasBeenCalled());
         }
     }
 
     @Test
     @UseReporter(UseReporterTest.TestReporter.class)
     void verifyWithoutReporter() {
-        Options options = new Options();
-        try {
-            Approvals.verify("hallo", options);
-        } catch (Error error) {
-            // check that getReporter was called
-            Asserts.assertEqual("the getReporter should've been called", "hallo\n", UseReporterTest.TestReporter.getLast());
-        }
+        FirstWorkingReporter reporter = (FirstWorkingReporter) new Options().getReporter();
+        assertEquals(UseReporterTest.TestReporter.class, reporter.getReporters()[1].getClass());
+    }
+
+    @Disabled("fixing previous test first")
+    @Test
+    void testTheVerifyApi() {
+        Method[] declaredMethods = Approvals.class.getDeclaredMethods();
+        List<Method> methodList = Query.where(declaredMethods, m -> m.getName().startsWith("verify"));
+        Approvals.verifyAll("verifyMethods", methodList, m -> m.toString());
     }
 
     static class ApprovalFailureReporterSpy implements ApprovalFailureReporter {
+
         private boolean hasBeenCalled;
 
         public ApprovalFailureReporterSpy() {
