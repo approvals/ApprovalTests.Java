5f0e529b3a38bd626078a00a98462dfbd7cdace3
Lars Eckart <lars.eckart@gmail.com>
2020-09-01 10:58:31 +0300

    R removed inheritance

--- approvaltests/src/main/java/org/approvaltests/combinations/pairwise/AppleSauce.java
+++ approvaltests/src/main/java/org/approvaltests/combinations/pairwise/AppleSauce.java
@@ -1,13 +1,15 @@
 package org.approvaltests.combinations.pairwise;
 
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Objects;
 import java.util.Set;
 
-public class AppleSauce extends LinkedHashMap<String, Object> implements Cloneable, Case1
+public class AppleSauce implements Case1
 {
+  private LinkedHashMap map = new LinkedHashMap();
   public AppleSauce()
   {
     super();
@@ -31,33 +33,64 @@ public class AppleSauce extends LinkedHashMap<String, Object> implements Cloneab
     keys.retainAll(pair.keySet());
     return !keys.stream().filter(key -> nonEquals(key, this, pair)).findFirst().isPresent();
   }
+  @Override
+  public Object put(String key, Object value)
+  {
+    return this.map.put(key, value);
+  }
+  @Override
+  public Collection<String> keySet()
+  {
+    return this.map.keySet();
+  }
+  @Override
+  public Set<Map.Entry<String, Object>> entrySet()
+  {
+    return this.map.entrySet();
+  }
   public Object get(int i)
   {
     return this.get(String.valueOf(i + 1));
   }
+  @Override
+  public Object get(Object key)
+  {
+    return this.map.get(key);
+  }
+  @Override
+  public void putAll(Map<? extends String, ?> pair)
+  {
+    this.map.putAll(pair);
+  }
   public AppleSauce union(AppleSauce pair)
   {
-    this.putAll(pair);
+    this.putAll(pair.map);
     return this;
   }
   @Override
   public AppleSauce clone()
   {
-    return new AppleSauce(this);
+    return new AppleSauce(this.map);
   }
-
-  @Override public boolean matches(Case1 pair) {
-    return matches((AppleSauce)pair);
+  @Override
+  public boolean matches(Case1 pair)
+  {
+    return matches((AppleSauce) pair);
   }
-
-  @Override public Case1 union(Case1 pair) {
+  @Override
+  public Case1 union(Case1 pair)
+  {
     return union((AppleSauce) pair);
   }
-
   private static boolean nonEquals(String key, AppleSauce first, AppleSauce second)
   {
     final Object f = first.get(key);
     final Object s = second.get(key);
     return Objects.nonNull(f) && Objects.nonNull(s) && !f.equals(s);
+  }
+  @Override
+  public String toString()
+  {
+    return map.toString();
   }
 }
