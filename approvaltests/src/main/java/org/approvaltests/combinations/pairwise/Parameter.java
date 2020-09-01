345459e3cebd1a826354a70fb22dd8a107da8bfb
Lars Eckart <lars.eckart@gmail.com>
2020-09-01 08:53:13 +0300

    r   Delete Clutter

--- approvaltests/src/main/java/org/approvaltests/combinations/pairwise/Parameter.java
+++ approvaltests/src/main/java/org/approvaltests/combinations/pairwise/Parameter.java
@@ -1,47 +1,38 @@
 package org.approvaltests.combinations.pairwise;
 
+import java.util.Arrays;
+
 public class Parameter<T> implements Parameter1
 {
-
-  private T[] arr;
-  public String getName()
-  {
-    return name;
-  }
-
-  @Override public T[] toArray() {
-    return arr;
-  }
-
-  @Override public int size() {
-    return arr.length;
-  }
-
-  @Override public T get(int index) {
-    return arr[index];
-  }
-
-  public void setName(String name)
-  {
-    this.name = name;
-  }
-  private String name;
-  public Parameter()
-  {
-    super();
-  }
-  public Parameter(String name)
-  {
-    this.name = name;
-  }
+  private final String name;
+  private final T[] arr;
   public Parameter(int position, T... values)
   {
     this.name = "" + (position + 1);
     this.arr = values;
   }
+  public String getName()
+  {
+    return name;
+  }
+  @Override
+  public T[] toArray()
+  {
+    return arr;
+  }
+  @Override
+  public int size()
+  {
+    return arr.length;
+  }
+  @Override
+  public T get(int index)
+  {
+    return arr[index];
+  }
   @Override
   public String toString()
   {
-    return name + ": " + super.toString();
+    return name + ": " + Arrays.toString(this.arr);
   }
 }
