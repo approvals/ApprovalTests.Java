2978c0428c671200d292559b85041b22c63111fd
Lars Eckart <lars.eckart@gmail.com>
2020-11-16 23:45:58 +0200

    added everything besides 8

--- approvaltests/src/main/java/org/approvaltests/combinations/PairWiseApprovals.java
+++ approvaltests/src/main/java/org/approvaltests/combinations/PairWiseApprovals.java
@@ -27,7 +27,23 @@ public class PairWiseApprovals {
                 EMPTY,
                 EMPTY,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
+    }
+
+    public static <IN1, OUT> void verifyBestCoveringPairs(Function1<IN1, OUT> call,
+                                                          IN1[] parameters1, Options options) {
+        verifyBestCoveringPairs((n1, n2, n3, n4, n5, n6, n7, n8, n9) -> call.call(n1),
+                parameters1,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                options);
     }
 
     public static <IN1, IN2, OUT> void verifyBestCoveringPairs(Function2<IN1, IN2, OUT> call,
@@ -41,7 +57,23 @@ public class PairWiseApprovals {
                 EMPTY,
                 EMPTY,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
+    }
+
+    public static <IN1, IN2, OUT> void verifyBestCoveringPairs(Function2<IN1, IN2, OUT> call,
+                                                               IN1[] parameters1, IN2[] parameters2, Options options) {
+        verifyBestCoveringPairs((n1, n2, n3, n4, n5, n6, n7, n8, n9) -> call.call(n1, n2),
+                parameters1,
+                parameters2,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                options);
     }
 
     public static <IN1, IN2, IN3, OUT> void verifyBestCoveringPairs(Function3<IN1, IN2, IN3, OUT> call,
@@ -55,7 +87,23 @@ public class PairWiseApprovals {
                 EMPTY,
                 EMPTY,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
+    }
+
+    public static <IN1, IN2, IN3, OUT> void verifyBestCoveringPairs(Function3<IN1, IN2, IN3, OUT> call,
+                                                                    IN1[] parameters1, IN2[] parameters2, IN3[] parameters3, Options options) {
+        verifyBestCoveringPairs((n1, n2, n3, n4, n5, n6, n7, n8, n9) -> call.call(n1, n2, n3),
+                parameters1,
+                parameters2,
+                parameters3,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+               options);
     }
 
     public static <IN1, IN2, IN3, IN4, OUT> void verifyBestCoveringPairs(Function4<IN1, IN2, IN3, IN4, OUT> call,
@@ -69,7 +117,8 @@ public class PairWiseApprovals {
                 EMPTY,
                 EMPTY,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
     }
 
     public static <IN1, IN2, IN3, IN4, OUT> void verifyBestCoveringPairs(Function4<IN1, IN2, IN3, IN4, OUT> call,
@@ -98,7 +147,23 @@ public class PairWiseApprovals {
                 EMPTY,
                 EMPTY,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
+    }
+
+    public static <IN1, IN2, IN3, IN4, IN5, OUT> void verifyBestCoveringPairs(Function5<IN1, IN2, IN3, IN4, IN5, OUT> call,
+                                                                              IN1[] parameters1, IN2[] parameters2, IN3[] parameters3, IN4[] parameters4, IN5[] parameters5, Options options) {
+        verifyBestCoveringPairs((n1, n2, n3, n4, n5, n6, n7, n8, n9) -> call.call(n1, n2, n3, n4, n5),
+                parameters1,
+                parameters2,
+                parameters3,
+                parameters4,
+                parameters5,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                options);
     }
 
     public static <IN1, IN2, IN3, IN4, IN5, IN6, OUT> void verifyBestCoveringPairs(Function6<IN1, IN2, IN3, IN4, IN5, IN6, OUT> call,
@@ -112,7 +177,22 @@ public class PairWiseApprovals {
                 parameters6,
                 EMPTY,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
+    }
+    public static <IN1, IN2, IN3, IN4, IN5, IN6, OUT> void verifyBestCoveringPairs(Function6<IN1, IN2, IN3, IN4, IN5, IN6, OUT> call,
+                                                                                   IN1[] parameters1, IN2[] parameters2, IN3[] parameters3, IN4[] parameters4, IN5[] parameters5, IN6[] parameters6, Options options) {
+        verifyBestCoveringPairs((n1, n2, n3, n4, n5, n6, n7, n8, n9) -> call.call(n1, n2, n3, n4, n5, n6),
+                parameters1,
+                parameters2,
+                parameters3,
+                parameters4,
+                parameters5,
+                parameters6,
+                EMPTY,
+                EMPTY,
+                EMPTY,
+                options);
     }
 
     public static <IN1, IN2, IN3, IN4, IN5, IN6, IN7, OUT> void verifyBestCoveringPairs(Function7<IN1, IN2, IN3, IN4, IN5, IN6, IN7, OUT> call,
@@ -126,7 +206,22 @@ public class PairWiseApprovals {
                 parameters6,
                 parameters7,
                 EMPTY,
-                EMPTY);
+                EMPTY,
+                new Options());
+    }
+    public static <IN1, IN2, IN3, IN4, IN5, IN6, IN7, OUT> void verifyBestCoveringPairs(Function7<IN1, IN2, IN3, IN4, IN5, IN6, IN7, OUT> call,
+                                                                                        IN1[] parameters1, IN2[] parameters2, IN3[] parameters3, IN4[] parameters4, IN5[] parameters5, IN6[] parameters6, IN7[] parameters7, Options options) {
+        verifyBestCoveringPairs((n1, n2, n3, n4, n5, n6, n7, n8, n9) -> call.call(n1, n2, n3, n4, n5, n6, n7),
+                parameters1,
+                parameters2,
+                parameters3,
+                parameters4,
+                parameters5,
+                parameters6,
+                parameters7,
+                EMPTY,
+                EMPTY,
+                options);
     }
     public static <IN1, IN2, IN3, IN4, IN5, IN6, IN7,IN8, OUT> void verifyBestCoveringPairs(Function8<IN1, IN2, IN3, IN4, IN5, IN6, IN7,IN8, OUT> call,
                                                                                             IN1[] parameters1, IN2[] parameters2, IN3[] parameters3, IN4[] parameters4, IN5[] parameters5, IN6[] parameters6, IN7[] parameters7, IN8[] parameters8) {
@@ -139,7 +234,8 @@ public class PairWiseApprovals {
                 parameters6,
                 parameters7,
                 parameters8,
-                EMPTY);
+                EMPTY,
+                new Options());
     }
 
     public static <IN1, IN2, IN3, IN4, IN5, IN6, IN7, IN8, IN9, OUT> void verifyBestCoveringPairs(
