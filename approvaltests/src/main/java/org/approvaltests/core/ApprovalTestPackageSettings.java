ef9d580928165466ccc065d77f00756f7e604e2f
Lars Eckart <lars.eckart@gmail.com>
2021-01-04 23:21:19 +0200

    r formatting

--- approvaltests/src/main/java/org/approvaltests/core/ApprovalTestPackageSettings.java
+++ approvaltests/src/main/java/org/approvaltests/core/ApprovalTestPackageSettings.java
@@ -5,10 +5,12 @@ import org.packagesettings.Field;
 
 public class ApprovalTestPackageSettings
 {
-  public static final Field<EnvironmentAwareReporter> FRONTLOADED_REPORTER = new Field<>("FrontloadedReporter",
-      EnvironmentAwareReporter.class);
-  public static final Field<ApprovalFailureReporter>  USE_REPORTER         = new Field<>("UseReporter",
+  public static final Field<EnvironmentAwareReporter> FRONTLOADED_REPORTER      = new Field<>(
+      "FrontloadedReporter", EnvironmentAwareReporter.class);
+  public static final Field<ApprovalFailureReporter>  USE_REPORTER              = new Field<>("UseReporter",
       ApprovalFailureReporter.class);
-    public static final Field<String> USE_APPROVAL_SUBDIRECTORY = new Field<>("UseApprovalSubdirectory", String.class);
-    public static final Field<String> APPROVAL_BASE_DIRECTORY = new Field<>("ApprovalBaseDirectory", String.class);
+  public static final Field<String>                   USE_APPROVAL_SUBDIRECTORY = new Field<>(
+      "UseApprovalSubdirectory", String.class);
+  public static final Field<String>                   APPROVAL_BASE_DIRECTORY   = new Field<>(
+      "ApprovalBaseDirectory", String.class);
 }
