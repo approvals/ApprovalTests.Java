3d4d918e4fe68b2a1c108837c1b11112b14afc27
Lars Eckart <lars.eckart@gmail.com>
2021-01-11 23:11:44 +0200

    F templates for discoverability of scrubbers

--- approvaltests/src/main/java/org/approvaltests/scrubbers/Scrubbers.java
+++ approvaltests/src/main/java/org/approvaltests/scrubbers/Scrubbers.java
@@ -6,4 +6,25 @@ public class Scrubbers
   {
     return new GuidScrubber().scrub(input);
   }
+  /**
+   * Provides methods that can be inlined as example code.
+   */
+  public static class Templates
+  {
+    /**
+     * To use this template, inline the method.
+     */
+    public static DateScrubber DateScrubber()
+    {
+      return DateScrubber.getScrubberFor("your date here: 2020-09-9T08:07Z");
+    }
+    public static GuidScrubber GuidScrubber()
+    {
+      return new GuidScrubber();
+    }
+    public static RegExScrubber RegExScrubber()
+    {
+      return new RegExScrubber("your pattern here: [a-zA-Z]+/d{4}", "<your replacement>");
+    }
+  }
 }
