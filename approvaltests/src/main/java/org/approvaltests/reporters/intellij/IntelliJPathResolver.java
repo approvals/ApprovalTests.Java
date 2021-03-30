6fe3a3c7e1c23cc903094db0b4a368400856b22c
Lars Eckart <lars.eckart@hey.com>
2021-03-30 19:19:38 +0300

    r replace temp with query

--- approvaltests/src/main/java/org/approvaltests/reporters/intellij/IntelliJPathResolver.java
+++ approvaltests/src/main/java/org/approvaltests/reporters/intellij/IntelliJPathResolver.java
@@ -8,16 +8,32 @@ import java.nio.file.Paths;
 import java.util.Comparator;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import com.spun.util.SystemUtils;
-import java.util.stream.Stream;
 
 public class IntelliJPathResolver
 {
   private final String channelsPath;
   private final String runtimeSuffix;
-
   public IntelliJPathResolver(Edition edition)
+  {
+    if (SystemUtils.isWindowsEnviroment())
+    {
+      runtimeSuffix = "/bin/idea64.exe";
+    }
+    else if (SystemUtils.isMacEnviroment())
+    {
+      runtimeSuffix = "/IntelliJ IDEA.app/Contents/MacOS/idea";
+    }
+    else // Linux
+    {
+      runtimeSuffix = "/bin/idea.sh";
+    }
+    String toolboxPath = appData() + "/JetBrains/Toolbox";
+    this.channelsPath = toolboxPath + "/apps/" + edition.getDirectory() + "/ch-0/";
+  }
+  private String appData()
   {
     String appData = "";
     if (SystemUtils.isWindowsEnviroment())
@@ -34,20 +50,7 @@ public class IntelliJPathResolver
       appData = System.getenv("HOME");
       appData += "/.local/share";
     }
-    if (SystemUtils.isWindowsEnviroment())
-    {
-      runtimeSuffix = "/bin/idea64.exe";
-    }
-    else if (SystemUtils.isMacEnviroment())
-    {
-      runtimeSuffix = "/IntelliJ IDEA.app/Contents/MacOS/idea";
-    }
-    else // Linux
-    {
-      runtimeSuffix = "/bin/idea.sh";
-    }
-    String toolboxPath = appData + "/JetBrains/Toolbox";
-    this.channelsPath = toolboxPath + "/apps/" + edition.getDirectory() + "/ch-0/";
+    return appData;
   }
   public String findIt()
   {
@@ -63,7 +66,8 @@ public class IntelliJPathResolver
   }
   private Optional<Path> getIntelliJPath() throws IOException
   {
-    try (Stream<Path> walk = Files.walk(Paths.get(channelsPath), 1, FileVisitOption.FOLLOW_LINKS)) {
+    try (Stream<Path> walk = Files.walk(Paths.get(channelsPath), 1, FileVisitOption.FOLLOW_LINKS))
+    {
       return walk //
           .map(Path::getFileName) //
           .map(Objects::toString) //
