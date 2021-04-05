e69540421bd3889a8f79d3d9b49f65125fe7c36f
Lars Eckart <lars.eckart@hey.com>
2021-04-05 23:13:55 +0300

    t tests for IntellijPathResolver

---
+++ approvaltests/src/test/java/org/approvaltests/reporters/intellij/IntelliJPathResolverTest.java
@@ -0,0 +1,27 @@
+package org.approvaltests.reporters.intellij;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.core.Options;
+import org.approvaltests.core.Scrubber;
+import org.approvaltests.namer.NamerFactory;
+import org.approvaltests.reporters.GenericDiffReporter;
+import org.approvaltests.reporters.UseReporter;
+import org.approvaltests.scrubbers.RegExScrubber;
+import org.junit.jupiter.api.Test;
+
+class IntelliJPathResolverTest
+{
+  @UseReporter(IntelliJMacSiliconReporter.class)
+  @Test
+  void testDetectionOfIntellijOnDevMachines()
+  {
+    if (!NamerFactory.asMachineNameSpecificTest().isCurrentEnvironmentValidFor("Larss-Air.lan", "macbook13"))
+    { return; }
+    final GenericDiffReporter environmentAwareReporter = (GenericDiffReporter) new IntelliJReporter()
+        .getWorkingReportersForEnviroment().get(0);
+    final String[] commandLine = environmentAwareReporter.getCommandLine("r.txt", "a.txt");
+    // the moment it breaks and we are annoyed, start scrubbing.
+    final Scrubber scrubber = new RegExScrubber("", "<your replacement>");
+    Approvals.verify(commandLine[0], new Options(scrubber));
+  }
+}
