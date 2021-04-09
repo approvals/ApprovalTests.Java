f33da9a4d0424d69be8d4bc707c8e97cf487139c
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-04-09 17:41:53 +0000

    t   fixed starting conditions

--- approvaltests/src/test/java/org/approvaltests/namer/JUnit3StackTraceNamerTest.java
+++ approvaltests/src/test/java/org/approvaltests/namer/JUnit3StackTraceNamerTest.java
@@ -1,14 +1,12 @@
 package org.approvaltests.namer;
 
-import org.junit.Test;
-
 import junit.framework.TestCase;
 
 public class JUnit3StackTraceNamerTest extends TestCase
 {
-  @Test
   public void testGetApprovalName()
   {
+    NamerFactory.getAndClearAdditionalInformation();
     StackTraceNamerUtils.assertNamerForFramework("JUnit3StackTraceNamerTest", "testGetApprovalName");
   }
 }
