4d363839ccbd755ff2e4d4c6843cb508570559b3
GitHub Action <action@github.com>
2021-04-09 18:26:40 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/machine_specific_tests/ApprovalsTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/machine_specific_tests/ApprovalsTest.java
@@ -27,9 +27,9 @@ public class ApprovalsTest extends MachineSpecificTest
     tv.selectTime("3pm");
     AwtApprovals.verify(tv);
   }
-
   @Test
-  void customPanel() {
+  void customPanel()
+  {
     AwtApprovals.verify(new CustomPanel());
   }
 }
--- approvaltests-tests/src/test/java/org/approvaltests/machine_specific_tests/CustomPanel.java
+++ approvaltests-tests/src/test/java/org/approvaltests/machine_specific_tests/CustomPanel.java
@@ -5,25 +5,26 @@ import org.lambda.functions.Function2;
 import javax.swing.*;
 import java.awt.*;
 
-public class CustomPanel extends JPanel {
-    public CustomPanel() {
-       setPreferredSize(new Dimension(100,100));
-       setSize(getPreferredSize());
+public class CustomPanel extends JPanel
+{
+  public CustomPanel()
+  {
+    setPreferredSize(new Dimension(100, 100));
+    setSize(getPreferredSize());
+  }
+  @Override
+  public void paint(Graphics g)
+  {
+    int square = 0;
+    StringBuffer b = new StringBuffer();
+    for (int y1 = 0; y1 < 10; y1++)
+    {
+      for (int x1 = 0; x1 < 10; x1++)
+      {
+        g.setColor(square++ % 2 == 0 ? Color.BLACK : Color.white);
+        g.fillRect((x1 * 10), (y1 * 10), 10, 10);
+      }
+      square++;
     }
-
-    @Override
-    public void paint(Graphics g) {
-      int square = 0;
-        StringBuffer b = new StringBuffer();
-        for (int y1 = 0; y1 < 10; y1++)
-        {
-          for (int x1 = 0; x1 < 10; x1++)
-          {
-              g.setColor(square++ %2 ==0 ? Color.BLACK: Color.white);
-              g.fillRect((x1*10),(y1*10), 10,10);
-
-          }
-          square++;
-        }
-    }
+  }
 }
