57902510d460ad7a2a2db13e6f96a6d1efb860e6
Lars Eckart <lars.eckart@hey.com>
2021-04-13 00:09:18 +0300

    t disable test where we continue next week

--- approvaltests-tests/src/test/java/org/approvaltests/awt/ApprovalsTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/awt/ApprovalsTest.java
@@ -2,6 +2,7 @@ package org.approvaltests.awt;
 
 import org.approvaltests.reporters.ClipboardReporter;
 import org.approvaltests.reporters.UseReporter;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnJre;
@@ -17,7 +18,8 @@ public class ApprovalsTest
     final CustomPanel panel = new CustomPanel();
     AwtApprovals.verify(panel);
   }
-  @DisabledOnJre({JRE.JAVA_8})
+//  @DisabledOnJre({JRE.JAVA_8})
+  @Disabled("continue here next week")
   @Test
   void customPanelWithText()
   {
