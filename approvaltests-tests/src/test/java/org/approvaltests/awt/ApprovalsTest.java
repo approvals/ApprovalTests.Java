9101b412a7b3075c174d4ca8a9360c88c216ea06
Lars Eckart <lars.eckart@hey.com>
2021-04-12 23:24:18 +0300

    t enable awt on jdk 9 and 10 to find when it changed

--- approvaltests-tests/src/test/java/org/approvaltests/awt/ApprovalsTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/awt/ApprovalsTest.java
@@ -3,10 +3,13 @@ package org.approvaltests.awt;
 import org.approvaltests.reporters.ClipboardReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnJre;
+import org.junit.jupiter.api.condition.JRE;
 
 @UseReporter({ClipboardReporter.class})
 public class ApprovalsTest
 {
+  @EnabledOnJre({JRE.JAVA_11, JRE.JAVA_16, JRE.JAVA_15, JRE.JAVA_9, JRE.JAVA_10})
   @Test
   void customPanel()
   {
