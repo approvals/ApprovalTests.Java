213d5fa57717a924891a1a71b78ad972a7d702f2
Lars Eckart <lars.eckart@hey.com>
2021-04-12 23:28:51 +0300

    t separate tests for jdk8 and above

--- approvaltests-tests/src/test/java/org/approvaltests/awt/ApprovalsTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/awt/ApprovalsTest.java
@@ -3,15 +3,23 @@ package org.approvaltests.awt;
 import org.approvaltests.reporters.ClipboardReporter;
 import org.approvaltests.reporters.UseReporter;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledOnJre;
 import org.junit.jupiter.api.condition.EnabledOnJre;
 import org.junit.jupiter.api.condition.JRE;
 
 @UseReporter({ClipboardReporter.class})
 public class ApprovalsTest
 {
-  @EnabledOnJre({JRE.JAVA_11, JRE.JAVA_16, JRE.JAVA_15, JRE.JAVA_9, JRE.JAVA_10})
+  @DisabledOnJre({JRE.JAVA_8})
   @Test
   void customPanel()
+  {
+    final CustomPanel panel = new CustomPanel();
+    AwtApprovals.verify(panel);
+  }
+  @EnabledOnJre({JRE.JAVA_8})
+  @Test
+  void customPanelOnJre8()
   {
     final CustomPanel panel = new CustomPanel();
     AwtApprovals.verify(panel);
