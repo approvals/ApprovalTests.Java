b1aad2363ddfabfc44e88d13e067c1dca5c61eaa
Lars Eckart <lars.eckart@hey.com>
2021-04-19 22:37:12 +0300

    t move tests to right folder and repeat 10 times
    we also tested with 10k repeats, always passed

--- approvaltests-tests/src/test/java/org/approvaltests/namer/AttributeStackSelectorTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/namer/AttributeStackSelectorTest.java
@@ -1,8 +1,13 @@
 package org.approvaltests.namer;
 
-import com.spun.util.tests.TestUtils;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.approvaltests.Approvals;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
+
+import com.spun.util.ThreadUtils;
+import com.spun.util.tests.TestUtils;
 
 public class AttributeStackSelectorTest
 {
@@ -12,5 +17,34 @@ public class AttributeStackSelectorTest
     String[] methodNames = {"doStuff", "lambda$handleCallback$0"};
     Approvals.verifyAll("unroll lambda", methodNames,
         m -> String.format("%s -> %s", m, TestUtils.unrollLambda(m)));
+  }
+  @RepeatedTest(10)
+  public void selectElement() throws Exception
+  {
+    callUnusedMethods();
+    AttributeStackSelector selector = new AttributeStackSelector();
+    StackTraceElement[] stackTrace = ThreadUtils.getStackTrace();
+    assertEquals(stackTrace[2], selector.selectElement(stackTrace));
+  }
+  private void callUnusedMethods()
+  {
+    selectElement("");
+    selectElement(0);
+    selectElement((AttributeStackSelectorTest) null);
+    selectElement((StackTraceElement) null);
+  }
+  // these methods are overloads of the test method
+  // to simulate flaky failures
+  public void selectElement(String unused)
+  {
+  }
+  public void selectElement(Integer unused)
+  {
+  }
+  public void selectElement(AttributeStackSelectorTest unused)
+  {
+  }
+  public void selectElement(StackTraceElement unused)
+  {
   }
 }
