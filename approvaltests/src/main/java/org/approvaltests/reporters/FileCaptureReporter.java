e5baf13addb5603468c45c7024429ebf2d834d05
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-04-24 00:13:50 +0000

    B!! Configure git for github actions in FileCaptureReporter
    Co-Authored-By: jmasonlee <262853+jmasonlee@users.noreply.github.com>

--- approvaltests/src/main/java/org/approvaltests/reporters/FileCaptureReporter.java
+++ approvaltests/src/main/java/org/approvaltests/reporters/FileCaptureReporter.java
@@ -2,35 +2,43 @@ package org.approvaltests.reporters;
 
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.concurrent.Callable;
 
+import org.apache.commons.lang.StringUtils;
 import org.approvaltests.core.ApprovalFailureReporter;
 
 import com.spun.util.ObjectUtils;
 import com.spun.util.io.FileUtils;
 import com.spun.util.logger.SimpleLogger;
+import org.lambda.functions.Function0;
 
 public class FileCaptureReporter implements ApprovalFailureReporter
 {
-  private String message = "*** adding received file via FileCaptureReporter for further inspection";
+  private String             message = "*** adding received file via FileCaptureReporter for further inspection";
+  private Function0<Boolean> isGitRegistrationNeeded;
   public FileCaptureReporter()
   {
+    this("*** adding received file via FileCaptureReporter for further inspection",
+        () -> StringUtils.isNotEmpty(System.getenv("GITHUB_ACTIONS")));
   }
-  public FileCaptureReporter(String message)
+  public FileCaptureReporter(String message, Function0<Boolean> isGitRegistrationNeeded)
   {
     this.message = message;
+    this.isGitRegistrationNeeded = isGitRegistrationNeeded;
   }
   @Override
   public void report(String received, String approved)
   {
-    // TODO: continue here
-    // checkGitUserConfigured();
-    //        git config --local user.email "action@github.com"
-    //        git config --local user.name "GitHub Action"
+    if (isGitRegistrationNeeded.call())
+    {
+      run("git", "config", "--local", "user.email", "action@github.com");
+      run("git", "config", "--local", "user.name", "githubAction");
+    }
     run("git", "add", "--force", received);
     run("git", "commit", "-m", "'" + message + "'");
     run("git", "push");
   }
-  private void run(String... command)
+  public static void run(String... command)
   {
     SimpleLogger.event(Arrays.toString(command));
     try
