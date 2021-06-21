184fbd2ddb1fc2b72333a22a99dcb458411da36b
Lars Eckart <action@github.com>
2021-06-21 23:01:11 +0300

    F!! fluent syntax and factory method

--- approvaltests-tests/src/test/java/org/approvaltests/StoryBoardTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/StoryBoardTest.java
@@ -10,11 +10,15 @@ public class StoryBoardTest
   @Test
   void gameOfLife()
   {
-    final GameOfLife gameOfLife = new GameOfLife((x, y) -> y == 2 && 1 <= x && x <= 3);
-    final Storyboard storyboard = new Storyboard();
+    GameOfLife gameOfLife = new GameOfLife((x, y) -> y == 2 && 1 <= x && x <= 3);
+    StoryBoard storyboard = new StoryBoard();
     storyboard.add(gameOfLife);
     storyboard.addFrames(3, gameOfLife::advance);
     Approvals.verify(storyboard);
+    gameOfLife = new GameOfLife((x, y) -> y == 2 && 1 <= x && x <= 3);
+    Approvals.verify(StoryBoard.createSequence(gameOfLife, 3, gameOfLife::advance));
+    gameOfLife = new GameOfLife((x, y) -> y == 2 && 1 <= x && x <= 3);
+    Approvals.verify(new StoryBoard().add(gameOfLife).addFrames(3, gameOfLife::advance));
   }
   private class GameOfLife
   {
@@ -27,18 +31,11 @@ public class StoryBoardTest
     {
       final Function2<Integer, Integer, Boolean> old = this.board;
       this.board = (x, y) -> {
-        final int aliveNeighbours = Queryable.as(
-            old.call(x - 1, y - 1),
-            old.call(x - 0, y - 1),
-            old.call(x + 1, y - 1),
-            old.call(x - 1, y - 0),
-//            old.call(x - 0, y - 0),
-            old.call(x + 1, y - 0),
-            old.call(x - 1, y + 1),
-            old.call(x - 0, y + 1),
-            old.call(x + 1, y + 1)
-        ).where(a -> a)
-            .size();
+        final int aliveNeighbours = Queryable
+            .as(old.call(x - 1, y - 1), old.call(x - 0, y - 1), old.call(x + 1, y - 1), old.call(x - 1, y - 0),
+                //            old.call(x - 0, y - 0),
+                old.call(x + 1, y - 0), old.call(x - 1, y + 1), old.call(x - 0, y + 1), old.call(x + 1, y + 1))
+            .where(a -> a).size();
         return aliveNeighbours == 3 || (aliveNeighbours == 2 && old.call(x, y));
       };
       return this;
