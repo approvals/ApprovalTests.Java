d0c602091eabb8d6fe645b9df5e45aa3f60e12e5
Scott Wierschem <scott@KeepCalmAndRefactor.com>
2021-07-15 15:47:56 -0500

    dt Comparing Stream and Query

--- approvaltests-util-tests/src/test/java/org/lambda/query/QueryTest.java
+++ approvaltests-util-tests/src/test/java/org/lambda/query/QueryTest.java
@@ -1,5 +1,7 @@
 package org.lambda.query;
 
+import org.approvaltests.legacycode.Range;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
@@ -22,5 +24,29 @@ public class QueryTest
     // None of the cases return false for empty data sets
     List<Integer> numbers = Arrays.asList();
     assertEquals(true, Query.all(numbers, n -> 3 <= n));
+  }
+
+  @Test
+  void queryVsStreamExample() {
+    // begin-snippet: query_example
+    Integer[] numbers = Range.get(1, 20);
+    Integer[] evenQueryNumbers = Query
+            .where(numbers, n -> n % 2 == 0)
+            .orderBy(OrderBy.Order.Descending,  n -> n)
+            .asArray();
+    // end-snippet
+    // begin-snippet: stream_example
+    Integer[] evenStreamNumbers = Arrays.stream(numbers)
+            .filter(n -> n % 2 == 0)
+            .sorted((o1, o2) -> o2.compareTo(o1))
+            .toArray(Integer[]::new);
+    // end-snippet
+    Assertions.assertArrayEquals(evenQueryNumbers, evenStreamNumbers);
+
+    int sumOfSquaresQuery = Query.sum(evenQueryNumbers, n -> n * n).intValue();
+    int sumOfSquaresStream = Arrays.stream(evenStreamNumbers).mapToInt(n -> n * n).sum();
+    int sumOfSquaresStream2 = Arrays.stream(evenStreamNumbers).map(n -> n * n).reduce(0, (a, b) -> a + b);
+    Assertions.assertEquals(sumOfSquaresQuery, sumOfSquaresStream);
+    Assertions.assertEquals(sumOfSquaresQuery, sumOfSquaresStream2);
   }
 }
--- approvaltests-util/docs/reference/Query.md
+++ approvaltests-util/docs/reference/Query.md
@@ -6,6 +6,24 @@
 ## Contents
 
   * [Extending Queryable](#extending-queryable)<!-- endToc -->
+## What it is
+Query (and the Queryable wrapper) is an alternative to Streams. In other words it is an implementation of Map/Reduce 
+based on the naming style of MS Linq.
+
+### Comparison vs. Streams
+Here is an example of filtering a list of numbers for only even numbers, sorted in reverse order:
+
+snippet: query_example
+
+Here is the exact same function but using Java Streams:
+
+snippet: stream_example
+
+#### Other benefits
+* Simpler interface
+* Queryable<T> extends List<T>
+* Queryable is customizable
+* Uses SQL syntax
 
 ## HowTos
 * [How to extend Queryable](../how_to/ExtendQueryable.md#top)
