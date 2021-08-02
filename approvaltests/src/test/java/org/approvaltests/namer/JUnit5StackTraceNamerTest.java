05f6e73d7919bc9e43d8544a77b7b19455032d78
Lars Eckart <action@github.com>
2021-08-03 00:18:27 +0300

    t update expected exception message

--- approvaltests/src/test/java/org/approvaltests/namer/JUnit5StackTraceNamerTest.java
+++ approvaltests/src/test/java/org/approvaltests/namer/JUnit5StackTraceNamerTest.java
@@ -114,7 +114,7 @@ public class JUnit5StackTraceNamerTest
       }
       catch (RuntimeException e)
       {
-        assertEquals("Use dynamicApprovals(String, Executable) instead", e.getMessage());
+        assertEquals("When using dynamic tests and Approvals, you need to use org.approvaltests.integrations.junit5.JUnit5Approvals.dynamicTest(String, Executable) instead.", e.getMessage());
       }
     }));
   }
