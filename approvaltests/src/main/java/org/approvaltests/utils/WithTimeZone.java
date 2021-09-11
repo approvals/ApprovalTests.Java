4489bcdd01deb1426a75035eb84a507301887586
Romano Zabini <romano.zabini@gmail.com>
2021-09-11 17:11:39 +0200

    B Make WithTimeZone thread safe
    Default time zone is global for a whole JVM instance, so a thread using WithTimeZone must be guaranteed that a concurrent thread do not change it in mid-execution.

--- approvaltests/src/main/java/org/approvaltests/utils/WithTimeZone.java
+++ approvaltests/src/main/java/org/approvaltests/utils/WithTimeZone.java
@@ -1,10 +1,12 @@
 package org.approvaltests.utils;
 
 import java.util.TimeZone;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class WithTimeZone implements AutoCloseable
 {
-  private final TimeZone tz;
+  private static final ReentrantLock defaultTimeZoneLock = new ReentrantLock();
+  private final TimeZone             tz;
   public WithTimeZone()
   {
     this("UTC");
@@ -12,11 +14,13 @@ public class WithTimeZone implements AutoCloseable
   public WithTimeZone(String zoneId)
   {
     tz = TimeZone.getDefault();
+    defaultTimeZoneLock.lock();
     TimeZone.setDefault(TimeZone.getTimeZone(zoneId));
   }
   @Override
   public void close()
   {
     TimeZone.setDefault(tz);
+    defaultTimeZoneLock.unlock();
   }
 }
