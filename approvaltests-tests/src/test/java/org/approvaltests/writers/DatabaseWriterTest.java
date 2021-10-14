062291e5d5408bcc82b2e68a2205811323374747
Scott Wierschem <scottw@potentialpower.com>
2021-10-14 16:31:45 -0500

    R Removed checked exceptions

--- approvaltests-tests/src/test/java/org/approvaltests/writers/DatabaseWriterTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/writers/DatabaseWriterTest.java
@@ -4,6 +4,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
+import com.spun.util.ObjectUtils;
 import org.approvaltests.Approvals;
 import org.easymock.EasyMock;
 import org.junit.jupiter.api.Test;
@@ -14,48 +15,49 @@ import com.spun.util.io.FileUtils;
 public class DatabaseWriterTest
 {
   @Test
-  public void testSimpleQuery() throws Exception
+  public void testSimpleQuery()
   {
     ResultSet rs = mockResultSetFromFile("query.csv");
     Approvals.verify(rs);
   }
   @Test
-  public void testSimpleQuery2() throws Exception
+  public void testSimpleQuery2()
   {
     ResultSet rs = queryDatabase();
     Approvals.verify(rs);
   }
-  public ResultSet queryDatabase() throws SQLException
+  public ResultSet queryDatabase()
   {
     return mockResultSetFromFile("sample_result_set.csv");
   }
-  private ResultSet mockResultSetFromFile(String fileName) throws SQLException
+  private ResultSet mockResultSetFromFile(String fileName)
   {
     String resultSet = FileUtils.readFromClassPath(getClass(), fileName).trim();
     String[][] data = CommaDelimitedFileParser.parse(resultSet);
     return mockResultSet(data);
   }
-  private ResultSet mockResultSet(String[][] data) throws SQLException
+  private ResultSet mockResultSet(String[][] data)
   {
-    ResultSet rs = EasyMock.createMock(ResultSet.class);
-    ResultSetMetaData metaData = EasyMock.createMock(ResultSetMetaData.class);
-    EasyMock.expect(rs.getMetaData()).andReturn(metaData).anyTimes();
-    EasyMock.expect(metaData.getColumnCount()).andReturn(data[0].length).anyTimes();
-    for (int i = 0; i < data[0].length; i++)
-    {
-      EasyMock.expect(metaData.getColumnName(i + 1)).andReturn(data[0][i]).anyTimes();
-    }
-    for (int i = 1; i < data.length; i++)
-    {
-      EasyMock.expect(rs.next()).andReturn(true);
-      for (int j = 0; j < data[i].length; j++)
-      {
-        EasyMock.expect(rs.getString(j + 1)).andReturn(data[i][j]);
+    try {
+      ResultSet rs = EasyMock.createMock(ResultSet.class);
+      ResultSetMetaData metaData = EasyMock.createMock(ResultSetMetaData.class);
+      EasyMock.expect(rs.getMetaData()).andReturn(metaData).anyTimes();
+      EasyMock.expect(metaData.getColumnCount()).andReturn(data[0].length).anyTimes();
+      for (int i = 0; i < data[0].length; i++) {
+        EasyMock.expect(metaData.getColumnName(i + 1)).andReturn(data[0][i]).anyTimes();
       }
+      for (int i = 1; i < data.length; i++) {
+        EasyMock.expect(rs.next()).andReturn(true);
+        for (int j = 0; j < data[i].length; j++) {
+          EasyMock.expect(rs.getString(j + 1)).andReturn(data[i][j]);
+        }
+      }
+      EasyMock.expect(rs.next()).andReturn(false);
+      EasyMock.replay(rs);
+      EasyMock.replay(metaData);
+      return rs;
+    } catch (SQLException e) {
+      throw ObjectUtils.throwAsError(e);
     }
-    EasyMock.expect(rs.next()).andReturn(false);
-    EasyMock.replay(rs);
-    EasyMock.replay(metaData);
-    return rs;
   }
 }
