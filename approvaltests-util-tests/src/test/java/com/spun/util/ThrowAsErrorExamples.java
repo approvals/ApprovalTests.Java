13ffea23988106ed98cc255344890b822d3449dc
GitHub Action <action@github.com>
2022-01-06 23:07:39 +0000

    a   reformat code

--- approvaltests-util-tests/src/test/java/com/spun/util/ThrowAsErrorExamples.java
+++ approvaltests-util-tests/src/test/java/com/spun/util/ThrowAsErrorExamples.java
@@ -4,42 +4,55 @@ import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-public class ThrowAsErrorExamples {
-    @Test
-    void examples() {
-
+public class ThrowAsErrorExamples
+{
+  @Test
+  void examples()
+  {
+  }
+  Integer whyReturnIsNeeded()
+  {
+    // begin-snippet: throw_as_error_does_not_compile
+    try
+    {
+      return methodThatThrowsCheckedException();
     }
-    Integer whyReturnIsNeeded() {
-        // begin-snippet: throw_as_error_does_not_compile
-        try {
-            return methodThatThrowsCheckedException();
-        } catch (Exception e) {
-            ObjectUtils.throwAsError(e); // Does not compile. Missing "return" statement
-        }
-        // end-snippet
-        return 0;
+    catch (Exception e)
+    {
+      ObjectUtils.throwAsError(e); // Does not compile. Missing "return" statement
     }
-    Integer withReturn() {
-        // begin-snippet: throw_as_error_with_return
-        try {
-            return methodThatThrowsCheckedException();
-        } catch (Exception e) {
-            ObjectUtils.throwAsError(e);
-            return null;  // This is never reached because the line above always throws an exception
-        }
-        // end-snippet
+    // end-snippet
+    return 0;
+  }
+  Integer withReturn()
+  {
+    // begin-snippet: throw_as_error_with_return
+    try
+    {
+      return methodThatThrowsCheckedException();
     }
-    Integer withThrow() {
-        // begin-snippet: throw_as_error_with_throw
-        try {
-            return methodThatThrowsCheckedException();
-        } catch (Exception e) {
-            throw ObjectUtils.throwAsError(e);
-        }
-        // end-snippet
+    catch (Exception e)
+    {
+      ObjectUtils.throwAsError(e);
+      return null; // This is never reached because the line above always throws an exception
     }
-
-    private Integer methodThatThrowsCheckedException() throws IOException {
-        return 1;
+    // end-snippet
+  }
+  Integer withThrow()
+  {
+    // begin-snippet: throw_as_error_with_throw
+    try
+    {
+      return methodThatThrowsCheckedException();
     }
+    catch (Exception e)
+    {
+      throw ObjectUtils.throwAsError(e);
+    }
+    // end-snippet
+  }
+  private Integer methodThatThrowsCheckedException() throws IOException
+  {
+    return 1;
+  }
 }
