ac72c19ed128ca3403541e0f8cbb19492fecedd9
GitHub Action <action@github.com>
2022-01-14 16:58:36 +0000

    a   reformat code

--- approvaltests/src/main/java/org/approvaltests/StoryBoard.java
+++ approvaltests/src/main/java/org/approvaltests/StoryBoard.java
@@ -4,10 +4,12 @@ import org.lambda.functions.Function0;
 
 public class StoryBoard
 {
-  enum Types  {None, Description, Frame}
-  private StringBuffer stringBuffer  = new StringBuffer();
-  int                  index         = 0;
-  private Types      last = Types.None;
+  enum Types {
+              None, Description, Frame
+  }
+  private StringBuffer stringBuffer = new StringBuffer();
+  int                  index        = 0;
+  private Types        last         = Types.None;
   public static <T> StoryBoard createSequence(T initial, int additionalFrames, Function0<T> getNextFrame)
   {
     return new StoryBoard().add(initial).addFrames(additionalFrames, getNextFrame);
@@ -20,28 +22,29 @@ public class StoryBoard
   public <T> StoryBoard addFrame(String title, T frame)
   {
     addNewLines(Types.Frame);
-    String frameTitle =  title;
+    String frameTitle = title;
     stringBuffer.append(String.format("%s:\n%s", frameTitle, frame));
     index++;
     return this;
   }
-
-  private void addNewLines(Types type) {
-    switch (last){
-      case None:
+  private void addNewLines(Types type)
+  {
+    switch (last)
+    {
+      case None :
         break;
-      case Description:
-        if (type == Types.Frame){
+      case Description :
+        if (type == Types.Frame)
+        {
           stringBuffer.append("\n\n");
         }
         break;
-      case Frame:
+      case Frame :
         stringBuffer.append("\n\n");
         break;
     }
     last = type;
   }
-
   public <T> StoryBoard addFrames(int howMany, Function0<T> getNextFrame)
   {
     for (int i = 0; i < howMany; i++)
@@ -58,7 +61,7 @@ public class StoryBoard
   public StoryBoard addDescription(String game_of_life)
   {
     addNewLines(Types.Description);
-    stringBuffer.append(game_of_life+"\n");
+    stringBuffer.append(game_of_life + "\n");
     return this;
   }
   public <T> StoryBoard addFrame(T frame)
