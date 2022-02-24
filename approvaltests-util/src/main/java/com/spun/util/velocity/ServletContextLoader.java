a90b95215de9040a4675d395870bcb58abacf35f
Scott Wierschem <scottw@potentialpower.com>
2022-02-24 16:28:01 -0600

    R!! Upgrading velocity to velocity.engine.core

--- approvaltests-util/src/main/java/com/spun/util/velocity/ServletContextLoader.java
+++ approvaltests-util/src/main/java/com/spun/util/velocity/ServletContextLoader.java
@@ -1,12 +1,14 @@
 package com.spun.util.velocity;
 
 import jakarta.servlet.ServletContext;
-import org.apache.commons.collections.ExtendedProperties;
 import org.apache.velocity.exception.ResourceNotFoundException;
 import org.apache.velocity.runtime.resource.Resource;
 import org.apache.velocity.runtime.resource.loader.ResourceLoader;
+import org.apache.velocity.util.ExtProperties;
 
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.util.Arrays;
 
 public class ServletContextLoader extends ResourceLoader
@@ -21,10 +23,10 @@ public class ServletContextLoader extends ResourceLoader
    *        been placed in the runtime's application attributes
    *        under its full class name (i.e. "javax.servlet.ServletContext").
    *
-   * @param configuration the {@link ExtendedProperties} associated with 
+   * @param configuration the {@link ExtProperties} associated with 
    *        this resource loader.
    */
-  public void init(ExtendedProperties configuration)
+  public void init(ExtProperties configuration)
   {
     paths = configuration.getStringArray("path");
     if (paths == null || paths.length == 0)
@@ -45,36 +47,38 @@ public class ServletContextLoader extends ResourceLoader
     }
     //My_System.variable("paths", paths);
   }
+
   /**
    * Get an InputStream so that the Runtime can build a
    * template with it.
    *
-   * @param name name of template to get
+   * @param source name of template to get
    * @return InputStream containing the template
    */
-  public synchronized InputStream getResourceStream(String name)
-  {
-    if (name == null || name.length() == 0)
+  @Override
+  public Reader getResourceReader(String source, String encoding) throws ResourceNotFoundException {
+    if (source == null || source.length() == 0)
     { return null; }
     /* since the paths always ends in '/',
      * make sure the name never ends in one */
-    while (name.startsWith("/"))
+    while (source.startsWith("/"))
     {
-      name = name.substring(1);
+      source = source.substring(1);
     }
     for (int i = 0; i < paths.length; i++)
     {
       InputStream result = null;
-      result = servletContext.getResourceAsStream(paths[i] + name);
+      result = servletContext.getResourceAsStream(paths[i] + source);
       if (result != null)
       {
         /* exit the loop if we found the template */
-        return result;
+        return new InputStreamReader(result);
       }
     }
     throw new ResourceNotFoundException(
-        String.format("Template '%s' not found from %s", name, Arrays.asList(paths)));
+            String.format("Template '%s' not found from %s", source, Arrays.asList(paths)));
   }
+
   /**
    * Defaults to return false.
    */
