18d0af02719f22c9803ded732f28965ec5717d4a
Lars Eckart <lars.eckart@hey.com>
2022-03-23 21:15:58 +0200

    t revert that test again
    we already test both windows and linux/mac with boolean flags

--- approvaltests-tests/src/test/java/org/approvaltests/reporters/ClipboardReporterTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/reporters/ClipboardReporterTest.java
@@ -1,28 +1,12 @@
 package org.approvaltests.reporters;
 
 import org.approvaltests.combinations.CombinationApprovals;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.condition.EnabledOnOs;
-
-import static org.junit.jupiter.api.condition.OS.LINUX;
-import static org.junit.jupiter.api.condition.OS.MAC;
-import static org.junit.jupiter.api.condition.OS.WINDOWS;
 
 class ClipboardReporterTest
 {
-  @EnabledOnOs({MAC, LINUX})
   @Test
   void commandLineMoves()
-  {
-    String[] files = {"with spaces.txt", "withoutSpaces.txt"};
-    Boolean[] systems = {true, false};
-    CombinationApprovals.verifyAllCombinations(ClipboardReporter::getAcceptApprovalText, files, files, systems);
-  }
-  @Disabled("TODO: find a windows user for this test")
-  @EnabledOnOs(WINDOWS)
-  @Test
-  void commandLineMovesWindows()
   {
     String[] files = {"with spaces.txt", "withoutSpaces.txt"};
     Boolean[] systems = {true, false};
