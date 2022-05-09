3105badb6c77d9eed417813db33f69341e585b5a
Lars Eckart <lars.eckart@hey.com>
2022-05-09 22:09:40 +0300

    r removed unused imports

--- approvaltests/src/main/java/org/approvaltests/reporters/macosx/KaleidoscopeDiffReporter.java
+++ approvaltests/src/main/java/org/approvaltests/reporters/macosx/KaleidoscopeDiffReporter.java
@@ -1,8 +1,6 @@
 package org.approvaltests.reporters.macosx;
 
-import org.approvaltests.reporters.DiffPrograms.Mac;
 import org.approvaltests.reporters.FirstWorkingReporter;
-import org.approvaltests.reporters.GenericDiffReporter;
 
 public class KaleidoscopeDiffReporter extends FirstWorkingReporter
 {
