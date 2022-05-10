2d3e5b8eac54020efba78c07dba317122cd7a20c
Lars Eckart <lars.eckart@hey.com>
2022-05-10 08:30:54 +0300

    t disable kaleidoscope 3 test as it is machine specific
    but did not fail in github actions?!

--- approvaltests/src/test/java/org/approvaltests/reporters/macosx/KaleidoscopeDiffReporterTest.java
+++ approvaltests/src/test/java/org/approvaltests/reporters/macosx/KaleidoscopeDiffReporterTest.java
@@ -2,10 +2,12 @@ package org.approvaltests.reporters.macosx;
 
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Options;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 class KaleidoscopeDiffReporterTest
 {
+  @Disabled("requires macOs and Kaleidoscope3 installed")
   @Test
   void kaleidoscope()
   {
