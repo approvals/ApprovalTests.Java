d25344f07bf4b318716c1ca56ad89339249bc0a0
Scott Wierschem <scottw@potentialpower.com>
2022-06-30 16:51:23 -0500

    t Added spec test for MarkdownTables

---
+++ approvaltests-util-tests/src/test/java/com/spun/util/markdown/table/MarkdownTableTest.java
@@ -0,0 +1,26 @@
+package com.spun.util.markdown.table;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.utils.VerifiableMarkdownTable;
+import org.junit.jupiter.api.Test;
+
+public class MarkdownTableTest {
+    @Test
+    void testRightJustified() {
+        VerifiableMarkdownTable table = VerifiableMarkdownTable.withHeaders("Number");
+        table.setColumnProperties(MarkdownColumn.RIGHT_JUSTIFIED);
+        for (int i = 9; i <= 11; i++) {
+            table.addRow(i);
+        }
+        Approvals.verify(table);
+    }
+    
+    @Test
+    void testDefaultJustification() {
+        VerifiableMarkdownTable table = VerifiableMarkdownTable.withHeaders("Number", "Number++");
+        for (int i = 9; i <= 11; i++) {
+            table.addRow(i, i+1);
+        }
+        Approvals.verify(table);
+    }
+}
