50630f5a2f2c07941c6a2982fa04ead8bc2f366c
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-08-01 21:34:52 -0600

    e compatible with macos

--- set_formatting_off.sh
+++ set_formatting_off.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-sed -i 's/<!-- formatter_plugin_begin -->/<!-- formatter_plugin_begin/g' pom.xml
-sed -i 's/<!-- formatter_plugin_end -->/ formatter_plugin_end -->/g' pom.xml
+sed -i '' 's/<!-- formatter_plugin_begin -->/<!-- formatter_plugin_begin/g' pom.xml
+sed -i '' 's/<!-- formatter_plugin_end -->/ formatter_plugin_end -->/g' pom.xml
         
