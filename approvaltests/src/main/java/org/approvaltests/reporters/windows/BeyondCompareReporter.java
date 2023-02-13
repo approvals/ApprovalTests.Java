2bdd9bfa7072eb7774b22073060fe49181de46e1
Lars Eckart <lars.eckart@hey.com>
2023-02-13 20:17:28 +0200

    B BeyondCompareReporter supports mac

--- approvaltests/src/main/java/org/approvaltests/reporters/windows/BeyondCompareReporter.java
+++ approvaltests/src/main/java/org/approvaltests/reporters/windows/BeyondCompareReporter.java
@@ -1,12 +1,13 @@
 package org.approvaltests.reporters.windows;
 
 import org.approvaltests.reporters.FirstWorkingReporter;
+import org.approvaltests.reporters.macosx.BeyondCompareMacReporter;
 
 public class BeyondCompareReporter extends FirstWorkingReporter
 {
   public static final BeyondCompareReporter INSTANCE = new BeyondCompareReporter();
   public BeyondCompareReporter()
   {
-    super(BeyondCompare4Reporter.INSTANCE, BeyondCompare3Reporter.INSTANCE);
+    super(BeyondCompare4Reporter.INSTANCE, BeyondCompare3Reporter.INSTANCE, BeyondCompareMacReporter.INSTANCE);
   }
 }
