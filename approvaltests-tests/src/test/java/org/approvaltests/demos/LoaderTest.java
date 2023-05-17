18d07d30d0848ac238cfda02b3fb82dbed7e4b10
GitHub Action <action@github.com>
2023-05-17 06:36:23 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/demos/LoaderTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/demos/LoaderTest.java
@@ -10,89 +10,98 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 @UseReporter(DiffMergeReporter.class)
-public class LoaderTest {
-    public static class Step1 {
-        // begin-snippet: separating_loaders_1
-        public void reserveItems(List<String> ids) {
-            Item[] items = getInventory();
-            for (Item item : items) {
-                if (ids.contains(item.id) && item.inventoryCount > 0) {
-                    registerHold(item);
-                }
-            }
+public class LoaderTest
+{
+  public static class Step1
+  {
+    // begin-snippet: separating_loaders_1
+    public void reserveItems(List<String> ids)
+    {
+      Item[] items = getInventory();
+      for (Item item : items)
+      {
+        if (ids.contains(item.id) && item.inventoryCount > 0)
+        {
+          registerHold(item);
         }
-        // end-snippet
+      }
     }
-
-    public static class Step2 {
-        // begin-snippet: separating_loaders_2
-        public void reserveItems(List<String> ids) {
-            reserveItems(ids, new InventoryLoader(), new ItemReserver());
+    // end-snippet
+  }
+  public static class Step2
+  {
+    // begin-snippet: separating_loaders_2
+    public void reserveItems(List<String> ids)
+    {
+      reserveItems(ids, new InventoryLoader(), new ItemReserver());
+    }
+    public void reserveItems(List<String> ids, Loader<Item[]> loader, Saver<Item> itemReserver)
+    {
+      Item[] items = loader.load();
+      for (Item item : items)
+      {
+        if (ids.contains(item.id) && item.inventoryCount > 0)
+        {
+          itemReserver.save(item);
         }
-
-        public void reserveItems(List<String> ids, Loader<Item[]> loader, Saver<Item> itemReserver) {
-            Item[] items = loader.load();
-            for (Item item : items) {
-                if (ids.contains(item.id) && item.inventoryCount > 0) {
-                    itemReserver.save(item);
-                }
-            }
-        }
-        // end-snippet
-        // begin-snippet: seperating_loaders_test
-
-        @Test
-        public void name() {
-
-            Item milk = new Item("M101", "Milk", 2);
-            Item missing_item = new Item("W202", "Item not Found", 2);
-            Item sold_out_item = new Item("S303", "SuperPopularGame", 0);
-
-            List<Item> saved = new ArrayList<>();
-            reserveItems(Arrays.asList(milk.id, missing_item.id, sold_out_item.id ),
-                    () -> new Item[]{milk, sold_out_item},
-                    i -> {saved.add(i); return i;});
-
-            // Only reserved milk
-            Assert.assertArrayEquals(saved.toArray(), new Item[]{milk});
-        }
-
-        // end-snippet
+      }
     }
-
-    public static class InventoryLoader implements Loader<Item[]> {
-        @Override
-        public Item[] load() {
-            return getInventory();
-        }
+    // end-snippet
+    // begin-snippet: seperating_loaders_test
+    @Test
+    public void name()
+    {
+      Item milk = new Item("M101", "Milk", 2);
+      Item missing_item = new Item("W202", "Item not Found", 2);
+      Item sold_out_item = new Item("S303", "SuperPopularGame", 0);
+      List<Item> saved = new ArrayList<>();
+      reserveItems(Arrays.asList(milk.id, missing_item.id, sold_out_item.id),
+          () -> new Item[]{milk, sold_out_item}, i -> {
+            saved.add(i);
+            return i;
+          });
+      // Only reserved milk
+      Assert.assertArrayEquals(saved.toArray(), new Item[]{milk});
     }
-
-    public static class ItemReserver implements Saver<Item> {
-        @Override
-        public Item save(Item save) {
-            registerHold(save);
-            return save;
-        }
+    // end-snippet
+  }
+  public static class InventoryLoader implements Loader<Item[]>
+  {
+    @Override
+    public Item[] load()
+    {
+      return getInventory();
     }
-
-    private static void registerHold(Item item) {
+  }
+  public static class ItemReserver implements Saver<Item>
+  {
+    @Override
+    public Item save(Item save)
+    {
+      registerHold(save);
+      return save;
     }
-
-    private static Item[] getInventory() {
-        Item[] items = new Item[0];
-        return items;
+  }
+  private static void registerHold(Item item)
+  {
+  }
+  private static Item[] getInventory()
+  {
+    Item[] items = new Item[0];
+    return items;
+  }
+  public static class Item
+  {
+    public String id;
+    public int    inventoryCount;
+    public String name;
+    public Item(String id, String name, int inventoryCount)
+    {
+      this.id = id;
+      this.inventoryCount = inventoryCount;
+      this.name = name;
     }
-
-    public static class Item {
-        public String id;
-        public int inventoryCount;
-        public String name;
-
-        public Item(String id, String name, int inventoryCount) {
-            this.id = id;
-            this.inventoryCount = inventoryCount;
-            this.name = name;
-        }
-    }
+  }
 }
