349d8dd0950b38f8b4c51eb602e22d2d7cf44c62
Scott Wierschem <scottw@potentialpower.com>
2023-10-05 16:35:13 -0500

    ! B Fixing missing approval file

--- approvaltests/src/main/java/org/approvaltests/reporters/KDiff3Reporter.java
+++ approvaltests/src/main/java/org/approvaltests/reporters/KDiff3Reporter.java
@@ -4,9 +4,11 @@ import org.approvaltests.reporters.linux.KDiff3LinuxReporter;
 import org.approvaltests.reporters.macosx.KDiff3MacReporter;
 import org.approvaltests.reporters.windows.KDiff3WindowsReporter;
 
-public class KDiff3Reporter extends FirstWorkingReporter {
-    public static final KDiff3Reporter INSTANCE = new KDiff3Reporter();
-    public KDiff3Reporter() {
-        super(KDiff3MacReporter.INSTANCE, KDiff3WindowsReporter.INSTANCE, KDiff3LinuxReporter.INSTANCE);
-    }
+public class KDiff3Reporter extends FirstWorkingReporter
+{
+  public static final KDiff3Reporter INSTANCE = new KDiff3Reporter();
+  public KDiff3Reporter()
+  {
+    super(KDiff3MacReporter.INSTANCE, KDiff3WindowsReporter.INSTANCE, KDiff3LinuxReporter.INSTANCE);
+  }
 }
