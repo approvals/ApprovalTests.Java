118cef4a5114c13b12cb32253d86ad558b9fe17b
Lars Eckart <lars.eckart@hey.com>
2023-10-09 20:52:37 +0300

    . t Change test to show paths we expect to be found

--- approvaltests-tests/src/test/java/org/approvaltests/reporters/IntelliJMacResolverTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/reporters/IntelliJMacResolverTest.java
@@ -1,19 +1,25 @@
 package org.approvaltests.reporters;
 
-import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 import org.lambda.query.Queryable;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 class IntelliJMacResolverTest
 {
   @Test
   void testFindIt()
   {
-    Queryable<String> q = new Queryable<>(String.class);
-    IntelliJMacResolver.getDiffInfo("user/lars", f -> {
-      q.add(f);
-      return false;
-    });
-    Approvals.verifyAll("Files", q);
+    String userHome = "Users/lars";
+    Queryable<String> validPaths = Queryable.as(
+        "/Applications/IntelliJ IDEA.app/Contents/MacOS/idea",
+        "Users/lars/Applications/IntelliJ IDEA Ultimate.app/Contents/MacOS/idea",
+        "Users/lars/Applications/IntelliJ IDEA Community.app/Contents/MacOS/idea",
+        "Users/lars/Applications/IntelliJ IDEA Community Edition.app/Contents/MacOS/idea");
+    for (String path : validPaths)
+    {
+      DiffInfo diffInfo = IntelliJMacResolver.getDiffInfo(userHome, f -> f.equals(path));
+      assertNotEquals("", diffInfo.diffProgram);
+    }
   }
 }
