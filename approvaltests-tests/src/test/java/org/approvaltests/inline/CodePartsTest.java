61b9af003920d2a867e0274e82beb4ffa6da7e6d
GitHub Action <action@github.com>
2023-11-22 20:20:58 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/inline/CodePartsTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/inline/CodePartsTest.java
@@ -3,28 +3,30 @@ package org.approvaltests.inline;
 import org.approvaltests.Approvals;
 import org.junit.jupiter.api.Test;
 
-class CodePartsTest {
-    @Test
-    public void testSplit() {
-        var code = """
-            public class Test {
-                @Test
-                void testyMctest() {
-                    Customer customer = Customer.create()
-                        .existingCustomer()
-                        .speakingEnglish()
-                        .build();
-                    stateService.save(TestUtils.userId().build(), customer);
-                    var expected = ""\"
-                        [Customer]: account lookup
-                        [     Bot]: Hi there!
-                        [     Bot]: Let me try to help.
-                        [     Bot]: routes to '12345'
-                        ""\";
-                    verifyConversation(expected, "account lookup");
-                }
+class CodePartsTest
+{
+  @Test
+  public void testSplit()
+  {
+    var code = """
+        public class Test {
+            @Test
+            void testyMctest() {
+                Customer customer = Customer.create()
+                    .existingCustomer()
+                    .speakingEnglish()
+                    .build();
+                stateService.save(TestUtils.userId().build(), customer);
+                var expected = ""\"
+                    [Customer]: account lookup
+                    [     Bot]: Hi there!
+                    [     Bot]: Let me try to help.
+                    [     Bot]: routes to '12345'
+                    ""\";
+                verifyConversation(expected, "account lookup");
             }
-            """;
-        Approvals.verify(CodeParts.splitCode(code, "testyMctest"));
-    }
+        }
+        """;
+    Approvals.verify(CodeParts.splitCode(code, "testyMctest"));
+  }
 }
--- approvaltests/src/main/java/org/approvaltests/inline/InlineJavaReporter.java
+++ approvaltests/src/main/java/org/approvaltests/inline/InlineJavaReporter.java
@@ -9,94 +9,98 @@ import java.io.File;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class InlineJavaReporter implements ApprovalFailureReporter, ApprovalReporterWithCleanUp {
-    private final String sourceFilePath;
-    private final StackTraceNamer stackTraceNamer;
-    private final ApprovalFailureReporter reporter;
-
-    public InlineJavaReporter(ApprovalFailureReporter reporter) {
-        this.reporter = reporter;
-        this.stackTraceNamer = new StackTraceNamer();
-        this.sourceFilePath = stackTraceNamer.getSourceFilePath();
+public class InlineJavaReporter implements ApprovalFailureReporter, ApprovalReporterWithCleanUp
+{
+  private final String                  sourceFilePath;
+  private final StackTraceNamer         stackTraceNamer;
+  private final ApprovalFailureReporter reporter;
+  public InlineJavaReporter(ApprovalFailureReporter reporter)
+  {
+    this.reporter = reporter;
+    this.stackTraceNamer = new StackTraceNamer();
+    this.sourceFilePath = stackTraceNamer.getSourceFilePath();
+  }
+  public String getSourceFilePath()
+  {
+    return sourceFilePath;
+  }
+  @Override
+  public boolean report(String received, String approved)
+  {
+    String sourceFile = sourceFilePath + stackTraceNamer.getInfo().getClassName() + ".java";
+    String newSource = createReceived(FileUtils.readFile(received));
+    return reporter.report(newSource, sourceFile);
+  }
+  public String createReceived(String actual)
+  {
+    String file = sourceFilePath + stackTraceNamer.getInfo().getClassName() + ".java";
+    String received = getReceivedFileName();
+    String text = FileUtils.readFile(file);
+    String fullText = createNewReceivedFileText(text, actual, this.stackTraceNamer.getInfo().getMethodName());
+    FileUtils.writeFile(new File(received), fullText);
+    return received;
+  }
+  private String getReceivedFileName()
+  {
+    return sourceFilePath + stackTraceNamer.getInfo().getClassName() + ".received.txt";
+  }
+  public static String createNewReceivedFileText(String text, String actual, String methodName)
+  {
+    text = text.replaceAll("\r\n", "\n");
+    CodeParts codeParts = CodeParts.splitCode(text, methodName);
+    if (codeParts.method.contains("expected = \"\"\""))
+    {
+      replaceExpected(codeParts, actual);
     }
-
-    public String getSourceFilePath() {
-        return sourceFilePath;
+    else
+    {
+      addExpected(codeParts, actual);
     }
-
-    @Override
-    public boolean report(String received, String approved) {
-        String sourceFile = sourceFilePath + stackTraceNamer.getInfo().getClassName() + ".java";
-        String newSource = createReceived(FileUtils.readFile(received));
-        return reporter.report(newSource, sourceFile);
+    return codeParts.getFullCode();
+  }
+  private static void addExpected(CodeParts codeParts, String actual)
+  {
+    int start = codeParts.method.indexOf("{") + 2;
+    String before = codeParts.method.substring(0, start);
+    String after = codeParts.method.substring(start);
+    codeParts.method = before + getExpected(actual, codeParts.tab) + after;
+  }
+  private static String getExpected(String actual, String tab)
+  {
+    return String.format("%s%svar expected = \"\"\"\n%s%s%s%s\"\"\";\n", tab, tab, indent(actual, tab), tab, tab,
+        tab);
+  }
+  private static void replaceExpected(CodeParts codeParts, String actual)
+  {
+    int start = codeParts.method.indexOf("expected = \"\"\"");
+    start = codeParts.method.substring(0, start).lastIndexOf("\n") + 1;
+    int end = codeParts.method.indexOf("\"\"\";");
+    end = codeParts.method.indexOf("\n", end) + 1;
+    String before = codeParts.method.substring(0, start);
+    String after = codeParts.method.substring(end);
+    codeParts.method = before + getExpected(actual, codeParts.tab) + after;
+  }
+  public static String indent(String actual, String tab)
+  {
+    String[] split = actual.split("\n");
+    String output = "";
+    for (String line : split)
+    {
+      output += tab + tab + tab + line + "\n";
     }
-
-    public String createReceived(String actual) {
-        String file = sourceFilePath + stackTraceNamer.getInfo().getClassName() + ".java";
-        String received = getReceivedFileName();
-        String text = FileUtils.readFile(file);
-        String fullText = createNewReceivedFileText(text, actual, this.stackTraceNamer.getInfo().getMethodName());
-        FileUtils.writeFile(new File(received), fullText);
-        return received;
-    }
-
-    private String getReceivedFileName() {
-        return sourceFilePath + stackTraceNamer.getInfo().getClassName() + ".received.txt";
-    }
-
-    public static String createNewReceivedFileText(String text, String actual, String methodName) {
-        text = text.replaceAll("\r\n", "\n");
-        CodeParts codeParts = CodeParts.splitCode(text, methodName);
-        if (codeParts.method.contains("expected = \"\"\"")) {
-            replaceExpected(codeParts, actual);
-        } else {
-            addExpected(codeParts, actual);
-        }
-        return codeParts.getFullCode();
-    }
-
-    private static void addExpected(CodeParts codeParts, String actual) {
-        int start = codeParts.method.indexOf("{") + 2;
-        String before = codeParts.method.substring(0, start);
-        String after = codeParts.method.substring(start);
-        codeParts.method = before + getExpected(actual, codeParts.tab) + after;
-    }
-
-    private static String getExpected(String actual, String tab) {
-        return String.format("%s%svar expected = \"\"\"\n%s%s%s%s\"\"\";\n",
-            tab, tab, indent(actual, tab), tab, tab, tab);
-    }
-
-    private static void replaceExpected(CodeParts codeParts, String actual) {
-        int start = codeParts.method.indexOf("expected = \"\"\"");
-        start = codeParts.method.substring(0, start).lastIndexOf("\n") + 1;
-        int end = codeParts.method.indexOf("\"\"\";");
-        end = codeParts.method.indexOf("\n", end) + 1;
-        String before = codeParts.method.substring(0, start);
-        String after = codeParts.method.substring(end);
-        codeParts.method = before + getExpected(actual, codeParts.tab) + after;
-    }
-
-    public static String indent(String actual, String tab) {
-        String[] split = actual.split("\n");
-        String output = "";
-        for (String line : split) {
-            output += tab + tab + tab + line + "\n";
-        }
-        return output;
-    }
-
-    private static String extractLeadingWhitespace(String text) {
-        Pattern pattern = Pattern.compile("^\\s+");
-        Matcher matcher = pattern.matcher(text);
-        if (matcher.find()) {
-            return matcher.group();
-        }
-        return "\t";
-    }
-
-    @Override
-    public void cleanUp(String received, String approved) {
-        FileUtils.delete(getReceivedFileName());
-    }
+    return output;
+  }
+  private static String extractLeadingWhitespace(String text)
+  {
+    Pattern pattern = Pattern.compile("^\\s+");
+    Matcher matcher = pattern.matcher(text);
+    if (matcher.find())
+    { return matcher.group(); }
+    return "\t";
+  }
+  @Override
+  public void cleanUp(String received, String approved)
+  {
+    FileUtils.delete(getReceivedFileName());
+  }
 }
