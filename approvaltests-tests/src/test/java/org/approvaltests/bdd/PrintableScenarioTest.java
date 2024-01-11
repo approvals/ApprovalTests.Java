942669765c556fd7a43fd66f08edc83ff251b2ac
GitHub Action <action@github.com>
2024-01-11 12:00:46 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/bdd/PrintableScenarioTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/bdd/PrintableScenarioTest.java
@@ -10,92 +10,93 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class PrintableScenarioTest {
-
-    @Test
-    public void printBDDScenario() throws Exception {
-        PrintableScenario story = new PrintableScenario("Buy Groceries", "Bob puts two items in the shopping cart");
-
-        User currentUser = new User();
-        ShoppingCart shoppingCart = new ShoppingCart();
-        story.given(
-                new Printable<>(currentUser, UserPrinter::print),
-                new Printable<>(shoppingCart, ShoppingCartPrinter::print)
-        );
-
-        story.when("Add oranges to the cart", () -> {
-            shoppingCart.add("Oranges", 1, BigDecimal.valueOf(5));
-            return null;
-        });
-
-        story.when("Add apples to the cart", () -> {
-            shoppingCart.add("Apples", 3, BigDecimal.valueOf(4));
-            return null;
-        });
-
-        Approvals.verify(story.then());
-    }
-
-
+public class PrintableScenarioTest
+{
+  @Test
+  public void printBDDScenario() throws Exception
+  {
+    PrintableScenario story = new PrintableScenario("Buy Groceries", "Bob puts two items in the shopping cart");
+    User currentUser = new User();
+    ShoppingCart shoppingCart = new ShoppingCart();
+    story.given(new Printable<>(currentUser, UserPrinter::print),
+        new Printable<>(shoppingCart, ShoppingCartPrinter::print));
+    story.when("Add oranges to the cart", () -> {
+      shoppingCart.add("Oranges", 1, BigDecimal.valueOf(5));
+      return null;
+    });
+    story.when("Add apples to the cart", () -> {
+      shoppingCart.add("Apples", 3, BigDecimal.valueOf(4));
+      return null;
+    });
+    Approvals.verify(story.then());
+  }
 }
 
-class User {
-    public String name = "Bob";
+class User
+{
+  public String name = "Bob";
 }
 
-class Item {
-    public String name;
-    public Integer quantity;
-    public BigDecimal price;
-
-    public BigDecimal amount() {
-        return price.multiply(BigDecimal.valueOf(quantity));
-    }
-    public Item(String itemName, int quantity, BigDecimal price) {
-        this.name = itemName;
-        this.quantity = quantity;
-        this.price = price;
-    }
-}
-class ShoppingCart {
-    public List<Item> articles = new ArrayList<>();
-    public BigDecimal subtotal() {
-        return articles.stream().map(Item::amount).reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
-    public BigDecimal shipping() {
-        return BigDecimal.ONE;
-    }
-
-    public BigDecimal total() {
-        return this.subtotal().add(this.shipping());
-    }
-
-    public void add(String itemName, int quantity, BigDecimal price) {
-        this.articles.add(new Item(itemName, quantity, price));
-    }
+class Item
+{
+  public String     name;
+  public Integer    quantity;
+  public BigDecimal price;
+  public BigDecimal amount()
+  {
+    return price.multiply(BigDecimal.valueOf(quantity));
+  }
+  public Item(String itemName, int quantity, BigDecimal price)
+  {
+    this.name = itemName;
+    this.quantity = quantity;
+    this.price = price;
+  }
 }
 
-class ShoppingCartPrinter {
-
-    public static String print(ShoppingCart shoppingCart) {
-        StringBuilder result = new StringBuilder();
-        result.append("ShoppingCart:\n");
-        result.append("Articles:\n");
-        result.append(shoppingCart.articles.stream()
-                .map(article -> "    " + article.name + " price: " + article.price + " quantity: " + article.quantity)
-                .collect(Collectors.joining("\n")));
-        result.append("\n");
-
-        result.append("Subtotal:" + shoppingCart.subtotal() + "\n");
-        result.append("Shipping:" + shoppingCart.shipping() + "\n");
-        result.append("Total:" + shoppingCart.total() + "\n");
-        return result.toString();
-    }
+class ShoppingCart
+{
+  public List<Item> articles = new ArrayList<>();
+  public BigDecimal subtotal()
+  {
+    return articles.stream().map(Item::amount).reduce(BigDecimal.ZERO, BigDecimal::add);
+  }
+  public BigDecimal shipping()
+  {
+    return BigDecimal.ONE;
+  }
+  public BigDecimal total()
+  {
+    return this.subtotal().add(this.shipping());
+  }
+  public void add(String itemName, int quantity, BigDecimal price)
+  {
+    this.articles.add(new Item(itemName, quantity, price));
+  }
 }
 
-class UserPrinter {
-    public static String print(User user) {
-        return "User: " + user.name + "\n";
-    }
+class ShoppingCartPrinter
+{
+  public static String print(ShoppingCart shoppingCart)
+  {
+    StringBuilder result = new StringBuilder();
+    result.append("ShoppingCart:\n");
+    result.append("Articles:\n");
+    result.append(shoppingCart.articles.stream()
+        .map(article -> "    " + article.name + " price: " + article.price + " quantity: " + article.quantity)
+        .collect(Collectors.joining("\n")));
+    result.append("\n");
+    result.append("Subtotal:" + shoppingCart.subtotal() + "\n");
+    result.append("Shipping:" + shoppingCart.shipping() + "\n");
+    result.append("Total:" + shoppingCart.total() + "\n");
+    return result.toString();
+  }
+}
+
+class UserPrinter
+{
+  public static String print(User user)
+  {
+    return "User: " + user.name + "\n";
+  }
 }
--- approvaltests/src/main/java/org/approvaltests/strings/PrintableScenario.java
+++ approvaltests/src/main/java/org/approvaltests/strings/PrintableScenario.java
@@ -5,67 +5,69 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.concurrent.Callable;
 
-public class PrintableScenario {
-
-
-    private final StringBuilder toVerify = new StringBuilder();
-    private final String name;
-    private final String description;
-    private final ArrayList<Printable> printables = new ArrayList<>();
-
-    public PrintableScenario(String name, String description) {
-        this.name = name;
-        this.description = description;
+public class PrintableScenario
+{
+  private final StringBuilder        toVerify   = new StringBuilder();
+  private final String               name;
+  private final String               description;
+  private final ArrayList<Printable> printables = new ArrayList<>();
+  public PrintableScenario(String name, String description)
+  {
+    this.name = name;
+    this.description = description;
+  }
+  public PrintableScenario(String name)
+  {
+    this(name, "");
+  }
+  public void given(Printable... printables)
+  {
+    arrange(printables);
+  }
+  public void arrange(Printable... printables)
+  {
+    this.printables.addAll(Arrays.asList(printables));
+    toVerify.append(makeHeading(name, description, "="));
+    toVerify.append("\n");
+    toVerify.append(printAll());
+  }
+  public String makeHeading(String heading, String summary, String underlineCharacter)
+  {
+    int count = heading.length();
+    // create a string made up of n copies of string "="
+    String underlineHeading = String.join("", Collections.nCopies(count, underlineCharacter));
+    return heading + "\n" + underlineHeading + "\n" + summary + "\n";
+  }
+  public String printAll()
+  {
+    StringBuilder result = new StringBuilder();
+    for (Printable printable : this.printables)
+    {
+      result.append(printable.toString());
+      result.append("\n");
     }
-    public PrintableScenario(String name) {
-        this(name, "");
-    }
-
-    public void given(Printable... printables) {
-        arrange(printables);
-    }
-
-    public void arrange(Printable... printables) {
-        this.printables.addAll(Arrays.asList(printables));
-        toVerify.append(makeHeading(name, description, "="));
-        toVerify.append("\n");
-        toVerify.append(printAll());
-    }
-
-    public String makeHeading(String heading, String summary, String underlineCharacter) {
-        int count = heading.length();
-        // create a string made up of n copies of string "="
-        String underlineHeading = String.join("", Collections.nCopies(count, underlineCharacter));
-        return heading + "\n" + underlineHeading + "\n" + summary + "\n";
-    }
-
-    public String printAll() {
-        StringBuilder result = new StringBuilder();
-        for (Printable printable : this.printables) {
-            result.append(printable.toString());
-            result.append("\n");
-        }
-        return result.toString();
-    }
-
-    public void when(String action, Callable function) throws Exception {
-        function.call();
-        when(action);
-    }
-
-    public void when(String action) {
-        act(action);
-    }
-
-    public void act(String action) {
-        toVerify.append(makeHeading(action, "", "-"));
-        toVerify.append(printAll());
-    }
-    public String then() {
-        return print();
-    }
-
-    public String print() {
-        return toVerify.toString();
-    }
+    return result.toString();
+  }
+  public void when(String action, Callable function) throws Exception
+  {
+    function.call();
+    when(action);
+  }
+  public void when(String action)
+  {
+    act(action);
+  }
+  public void act(String action)
+  {
+    toVerify.append(makeHeading(action, "", "-"));
+    toVerify.append(printAll());
+  }
+  public String then()
+  {
+    return print();
+  }
+  public String print()
+  {
+    return toVerify.toString();
+  }
 }
