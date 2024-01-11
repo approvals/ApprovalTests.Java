31c3825b50f37ff6d3102c3d67ab79a7980d520d
emily <emily@bacheconsulting.com>
2024-01-11 12:59:14 +0100

    Added PrintableScenario class for creating BDD scenarios with Approvals.

---
+++ approvaltests-tests/src/test/java/org/approvaltests/bdd/PrintableScenarioTest.java
@@ -0,0 +1,101 @@
+package org.approvaltests.bdd;
+
+import org.approvaltests.Approvals;
+import org.approvaltests.strings.Printable;
+import org.approvaltests.strings.PrintableScenario;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class PrintableScenarioTest {
+
+    @Test
+    public void printBDDScenario() throws Exception {
+        PrintableScenario story = new PrintableScenario("Buy Groceries", "Bob puts two items in the shopping cart");
+
+        User currentUser = new User();
+        ShoppingCart shoppingCart = new ShoppingCart();
+        story.given(
+                new Printable<>(currentUser, UserPrinter::print),
+                new Printable<>(shoppingCart, ShoppingCartPrinter::print)
+        );
+
+        story.when("Add oranges to the cart", () -> {
+            shoppingCart.add("Oranges", 1, BigDecimal.valueOf(5));
+            return null;
+        });
+
+        story.when("Add apples to the cart", () -> {
+            shoppingCart.add("Apples", 3, BigDecimal.valueOf(4));
+            return null;
+        });
+
+        Approvals.verify(story.then());
+    }
+
+
+}
+
+class User {
+    public String name = "Bob";
+}
+
+class Item {
+    public String name;
+    public Integer quantity;
+    public BigDecimal price;
+
+    public BigDecimal amount() {
+        return price.multiply(BigDecimal.valueOf(quantity));
+    }
+    public Item(String itemName, int quantity, BigDecimal price) {
+        this.name = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+class ShoppingCart {
+    public List<Item> articles = new ArrayList<>();
+    public BigDecimal subtotal() {
+        return articles.stream().map(Item::amount).reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    public BigDecimal shipping() {
+        return BigDecimal.ONE;
+    }
+
+    public BigDecimal total() {
+        return this.subtotal().add(this.shipping());
+    }
+
+    public void add(String itemName, int quantity, BigDecimal price) {
+        this.articles.add(new Item(itemName, quantity, price));
+    }
+}
+
+class ShoppingCartPrinter {
+
+    public static String print(ShoppingCart shoppingCart) {
+        StringBuilder result = new StringBuilder();
+        result.append("ShoppingCart:\n");
+        result.append("Articles:\n");
+        result.append(shoppingCart.articles.stream()
+                .map(article -> "    " + article.name + " price: " + article.price + " quantity: " + article.quantity)
+                .collect(Collectors.joining("\n")));
+        result.append("\n");
+
+        result.append("Subtotal:" + shoppingCart.subtotal() + "\n");
+        result.append("Shipping:" + shoppingCart.shipping() + "\n");
+        result.append("Total:" + shoppingCart.total() + "\n");
+        return result.toString();
+    }
+}
+
+class UserPrinter {
+    public static String print(User user) {
+        return "User: " + user.name + "\n";
+    }
+}
---
+++ approvaltests/docs/how_to/BddScenarios.md
@@ -0,0 +1,27 @@
+<a id="top"></a>
+
+# How to make a BDD Scenario using Approvals
+<!-- toc -->
+## Contents
+
+* [Introduction](#introduction)
+* [Sample Code](#sample-code)<!-- endToc -->
+
+## Introduction
+The idea is that you will end up with a descriptive approved file that reads like a BDD Scenario. You can show this to your less-technical collaborators, and they should be able to understand what scenario is being tested without needing to read the test sourcecode.
+
+It doesn't use Gherkin strictly, but you still get a scenario with a name, description, and Given, When, Then steps.
+
+## Sample Code
+
+I need some help with this part of the documentation. I'd like to include PrintableScenarioTest here as an example.
+<!-- snippet: bdd_test -->
+
+<!-- endSnippet -->
+
+* Construct your scenario at the beginning of the test with a descriptive name and summary.
+* When you are done with the code for the "given" step of the test, call story.given() with your list of Printables. Use one printable to wrap each object you have created which you think might change state during the test and that you want to verify.
+* In each "When" step of your test, call when() with a descriptive name for what the user is doing. The second argument is a lambda that contains code to actually do the when step. This is optional, you can write it in your test as normal code before the call to 'when' if you prefer.
+* The "Then" step is a call to then() which will verify the state of all the Printables as it changed throughout the course of the test.
+
+Note - if you don't want to use the BDD terminology you don't have to. You can equally well use 'arrange', 'act', 'print' instead of 'given' 'when' 'then'
