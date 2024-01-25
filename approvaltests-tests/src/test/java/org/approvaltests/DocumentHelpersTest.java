95250bc39bc2db545aef39b3aba731aa3ee55f48
Scott Wierschem <scottw@potentialpower.com>
2024-01-25 15:41:07 -0600

    . d Verify documentation links to call with Options

--- approvaltests-tests/src/test/java/org/approvaltests/DocumentHelpersTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/DocumentHelpersTest.java
@@ -4,39 +4,48 @@ import com.github.javaparser.Range;
 import com.spun.util.StringUtils;
 import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Test;
+import org.lambda.functions.Function1;
 import org.lambda.query.Query;
 import org.lambda.query.Queryable;
 
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
+import java.lang.reflect.Parameter;
 import java.util.List;
 
-public class DocumentHelpersTest
-{
+public class DocumentHelpersTest {
   @Test
-  void listAllVerifyFunctions()
-  {
+  void listAllVerifyFunctions() {
     Queryable<Method> methods = getAllVerifyFunctionsWithOptions(OptionsTest.getApprovalClasses());
     Queryable<String> lines = methods.select(m -> String.format("%s. [%s ](%s) (%s)",
-        m.getDeclaringClass().getSimpleName(), m.getName(), getLink(m), showParameters(m)))
-        .orderBy(s -> s.replaceAll("#L\\d+-L\\d+", ""));
+                    m.getDeclaringClass().getSimpleName(), m.getName(), getLink(m), showParametersExceptOptions(m)))
+            .orderBy(s -> s.replaceAll("#L\\d+-L\\d+", ""));
     Approvals.verifyAll("", lines, l -> String.format(" * %s  ", l), new Options().forFile().withExtension(".md"));
   }
+
   @Test
-  void testLineNumbers()
-  {
+  void testLineNumbers() {
     var expected = """
-      https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/src/main/java/org/approvaltests/Approvals.java#L98-L101
-      """;
+            https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/src/main/java/org/approvaltests/Approvals.java#L98-L101
+            """;
     String verifyAll = getLink(Query.first(Approvals.class.getMethods(),
             m -> m.getName().equals("verifyAll") && m.getParameterTypes()[0].equals(Object[].class)));
     Approvals.verify(verifyAll, new Options().inline(expected));
   }
-  public static String showParameters(Method m)
-  {
-    return StringUtils.join(Query.select(m.getParameters(), p -> String.format("%s", p.getType().getSimpleName())),
-        ",");
+
+  public static String showParameters(Method m) {
+    return showParametersExcept(m, p -> true);
   }
+  public static String showParametersExceptOptions(Method m)
+  {
+      return showParametersExcept(m, p -> !p.getType().getSimpleName().equals("Options"));
+  }
+
+  private static String showParametersExcept(Method m, Function1<Parameter, Boolean> filter) {
+    Queryable<Parameter> withoutOptions = Query.where(m.getParameters(), filter);
+    return StringUtils.join(withoutOptions.select(p -> String.format("%s", p.getType().getSimpleName())), ",");
+  }
+
   public static String getLink(Method m)
   {
     String baseUrl = "https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/src/main/java";
@@ -55,7 +64,7 @@ public class DocumentHelpersTest
       Queryable<Method> declaredMethods = Queryable.as(c.getDeclaredMethods());
       Queryable<Method> methodList = declaredMethods.where(m -> m.getName().startsWith("verify")
           && Modifier.isPublic(m.getModifiers()) && !m.isAnnotationPresent(Deprecated.class));
-      List<Method> methodsWithOptions = methodList.where(m -> !OptionsTest.isOptionsPresent(m));
+      List<Method> methodsWithOptions = methodList.where(m -> OptionsTest.isOptionsPresent(m));
       methods.addAll(methodsWithOptions);
     }
     return methods;
