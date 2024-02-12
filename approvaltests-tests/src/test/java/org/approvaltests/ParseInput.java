8c65e74933274e06c8f90c7bb53f4be8e435be77
GitHub Action <action@github.com>
2024-02-12 18:24:40 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/ParseInput.java
+++ approvaltests-tests/src/test/java/org/approvaltests/ParseInput.java
@@ -8,74 +8,57 @@ import org.lambda.query.Queryable;
 
 import java.util.HashMap;
 
-public class ParseInput <T>{
-
-    private String expected;
-    private final Function1<String, Tuple<String, T>> transformer;
-
-
-
-    public static ParseInput<String> create(String expected) {
-        return new ParseInput<String>(expected, s -> new Tuple<>(s,s));
-
-    }
-    public static<T> ParseInput<T> create(String expected,Function1<String, T> transformer) {
-        return new ParseInput<T>(expected,s -> new Tuple<>(s,transformer.call(s)));
-
-    }
-
-    private ParseInput(String expected, Function1<String, Tuple<String, T>> transformer) {
-        this.expected = expected;
-        this.transformer = transformer;
-    }
-
-    public static <T> ParseInput<T> createFromParts(String expected, Function1<Queryable<String>, T> transformer) {
-        return new ParseInput<T>(expected,s -> {
-            var temp = Queryable.as(s.split(",")).select(String::trim);
-            return new Tuple<>(s, transformer.call(temp));
-        });
-    }
-
-    public static <T>  ParseInput<T> create(String expected, Class<T> tranformTo) {
-        var transformers = new HashMap<Class<?>, Function1<String,Object>>()
-                {{
-                    put(Integer.class, s -> Integer.parseInt(s));
-                    put(String.class, s -> s);
-                    put(Double.class, s -> Double.parseDouble(s));
-                    put(Boolean.class, s -> Boolean.parseBoolean(s));
-                    put(Long.class, s -> Long.parseLong(s));
-                    put(Float.class, s -> Float.parseFloat(s));
-                    put(Short.class, s -> Short.parseShort(s));
-                }}
-        ;
-        return ParseInput.create(expected, (Function1<String, T>) transformers.get(tranformTo));
-    }
-
-    public Queryable<Tuple<String, T>> parse(String expected) {
-        return Queryable.as(expected.lines())
-                .select(l -> l.split("->")[0].trim())
-                .select(l -> transformer.call(l));
-    }
-
-    public String print(String input, Object output) {
-        return input + " -> " + output;
-    }
-
-    public void verifyAll(Function1<T, Object> transform) {
-        Approvals.verifyAll("", parse(expected),
-                s -> print(s.getFirst(), transform.call(s.getSecond())), new Options().inline(expected));
-    }
+public class ParseInput<T>
+{
+  private String                                    expected;
+  private final Function1<String, Tuple<String, T>> transformer;
+  public static ParseInput<String> create(String expected)
+  {
+    return new ParseInput<String>(expected, s -> new Tuple<>(s, s));
+  }
+  public static <T> ParseInput<T> create(String expected, Function1<String, T> transformer)
+  {
+    return new ParseInput<T>(expected, s -> new Tuple<>(s, transformer.call(s)));
+  }
+  private ParseInput(String expected, Function1<String, Tuple<String, T>> transformer)
+  {
+    this.expected = expected;
+    this.transformer = transformer;
+  }
+  public static <T> ParseInput<T> createFromParts(String expected, Function1<Queryable<String>, T> transformer)
+  {
+    return new ParseInput<T>(expected, s -> {
+      var temp = Queryable.as(s.split(",")).select(String::trim);
+      return new Tuple<>(s, transformer.call(temp));
+    });
+  }
+  public static <T> ParseInput<T> create(String expected, Class<T> tranformTo)
+  {
+    var transformers = new HashMap<Class<?>, Function1<String, Object>>()
+    {
+      {
+        put(Integer.class, s -> Integer.parseInt(s));
+        put(String.class, s -> s);
+        put(Double.class, s -> Double.parseDouble(s));
+        put(Boolean.class, s -> Boolean.parseBoolean(s));
+        put(Long.class, s -> Long.parseLong(s));
+        put(Float.class, s -> Float.parseFloat(s));
+        put(Short.class, s -> Short.parseShort(s));
+      }
+    };
+    return ParseInput.create(expected, (Function1<String, T>) transformers.get(tranformTo));
+  }
+  public Queryable<Tuple<String, T>> parse(String expected)
+  {
+    return Queryable.as(expected.lines()).select(l -> l.split("->")[0].trim()).select(l -> transformer.call(l));
+  }
+  public String print(String input, Object output)
+  {
+    return input + " -> " + output;
+  }
+  public void verifyAll(Function1<T, Object> transform)
+  {
+    Approvals.verifyAll("", parse(expected), s -> print(s.getFirst(), transform.call(s.getSecond())),
+        new Options().inline(expected));
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
--- approvaltests-tests/src/test/java/org/approvaltests/ParseInputTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/ParseInputTest.java
@@ -2,67 +2,57 @@ package org.approvaltests;
 
 import org.junit.jupiter.api.Test;
 
-public class ParseInputTest {
-
-    @Test
-    void uppercasse() {
-        var expected = """
-                a -> A
-                b -> B
-                hh -> HH
-                c -> C
-                d -> D
-                e -> E
-                cco -> CCO
-                f -> F
-                g -> G
-                eue -> EUE
-                aza -> AZA
-                """;
-        ParseInput.create(expected).verifyAll(s -> s.toUpperCase());
-
-    }
-
-    @Test
-    void toBinary() {
-        var expected = """
-                1 -> 1
-                2 -> 10
-                4 -> 100
-                8 -> 1000
-                9 -> 1001
-                """;
-
-        ParseInput.create(expected, Integer::parseInt)
-                .verifyAll(s -> Integer.toBinaryString(s));
-
-    }
-
-    @Test
-    void toHex() {
-        var expected = """
-            1 -> 1
-            10 -> a
-            16 -> 10
-            22 -> 16
-            """;
-
-        ParseInput.create(expected, Integer.class)
-                .verifyAll(s -> Integer.toHexString(s));
-
-    }
-
-    @Test
-    void testPeople() {
-        var expected = """
-                Llewellyn, 25 -> adult
-                Oliver, 15 -> teenager    
-                """;
-
-        Creator<Person> createPerson = (a) -> new Person(a.get(0), Integer.parseInt(a.get(1)));
-        ParseInput.createFromParts(expected, createPerson)
-                .verifyAll(s -> s.getAgeLabel());
-
-    }
-
+public class ParseInputTest
+{
+  @Test
+  void uppercasse()
+  {
+    var expected = """
+        a -> A
+        b -> B
+        hh -> HH
+        c -> C
+        d -> D
+        e -> E
+        cco -> CCO
+        f -> F
+        g -> G
+        eue -> EUE
+        aza -> AZA
+        """;
+    ParseInput.create(expected).verifyAll(s -> s.toUpperCase());
+  }
+  @Test
+  void toBinary()
+  {
+    var expected = """
+        1 -> 1
+        2 -> 10
+        4 -> 100
+        8 -> 1000
+        9 -> 1001
+        """;
+    ParseInput.create(expected, Integer::parseInt).verifyAll(s -> Integer.toBinaryString(s));
+  }
+  @Test
+  void toHex()
+  {
+    var expected = """
+        1 -> 1
+        10 -> a
+        16 -> 10
+        22 -> 16
+        """;
+    ParseInput.create(expected, Integer.class).verifyAll(s -> Integer.toHexString(s));
+  }
+  @Test
+  void testPeople()
+  {
+    var expected = """
+        Llewellyn, 25 -> adult
+        Oliver, 15 -> teenager
+        """;
+    Creator<Person> createPerson = (a) -> new Person(a.get(0), Integer.parseInt(a.get(1)));
+    ParseInput.createFromParts(expected, createPerson).verifyAll(s -> s.getAgeLabel());
+  }
 }
