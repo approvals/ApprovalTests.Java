52ab4cedc584f7be0fd79a777f0462d94c015cf8
Llewellyn Falco <llewellyn.falco@gmail.com>
2024-02-12 17:09:36 -0700

    - F skip blank lines

--- approvaltests-tests/src/test/java/org/approvaltests/ParseInput.java
+++ approvaltests-tests/src/test/java/org/approvaltests/ParseInput.java
@@ -73,7 +73,10 @@ public class ParseInput<T>
 
   public Queryable<Tuple<String, T>> parse(String expected)
   {
-    return Queryable.as(expected.lines()).select(l -> l.split("->")[0].trim()).select(l -> transformer.call(l));
+    return Queryable.as(expected.lines())
+            .select(l -> l.split("->")[0].trim())
+            .where(l -> !l.isEmpty())
+            .select(l -> transformer.call(l));
   }
   public String print(String input, Object output)
   {
