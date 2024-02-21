88c52ff56597bc2ecaae3e8a46cf5a68751e480e
GitHub Action <action@github.com>
2024-02-21 21:19:10 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/writers/ApprovalXmlWriterTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/writers/ApprovalXmlWriterTest.java
@@ -11,72 +11,67 @@ import org.junit.jupiter.api.Test;
 
 import static com.fasterxml.jackson.dataformat.xml.ser.ToXmlGenerator.Feature.WRITE_XML_DECLARATION;
 
-public class ApprovalXmlWriterTest {
-    @Test
-    public void plainXml() {
-        Approvals.verifyXml("<xml><hello/><start>hi</start></xml>");
+public class ApprovalXmlWriterTest
+{
+  @Test
+  public void plainXml()
+  {
+    Approvals.verifyXml("<xml><hello/><start>hi</start></xml>");
+  }
+  @Test
+  public void xmlWithAttributes()
+  {
+    Approvals.verifyXml("<xml b=\"123\" a=\"456\"><hello x=\"y\"/><start>hi</start></xml>");
+  }
+  @Test
+  public void xmlWithDeepAttributes()
+  {
+    Approvals
+        .verifyXml("<xml b=\"1\" a=\"1\"><branch1 b=\"1\" a=\"1\"/><branch2 b=\"1\" a=\"1\">hi</branch2></xml>");
+  }
+  @Test
+  public void xmlWithDeepAttributesWithScrubber()
+  {
+    Approvals.verifyXml(
+        "<xml b=\"1\" a=\"1\"><branch1 b=\"1\" a=\"1\"/><branch2 b=\"1\" a=\"1\">hi</branch2></xml>",
+        new Options(new RegExScrubber("hi", "hello")));
+  }
+  @Test
+  public void invalidXml()
+  {
+    Approvals.verifyXml("<xml><hello/><start>hi</xml>");
+    System.err.println("Note: The previous xml error (</start>) is expected ");
+  }
+  @Test
+  void xmlOutputByJacksonIsHandledCorrectly() throws JsonProcessingException
+  {
+    ObjectWriter objectWriter = new XmlMapper().configure(WRITE_XML_DECLARATION, true).writer();
+    String jacksonXml = objectWriter.writeValueAsString(new JacksonTestPOJO());
+    String approvalsWriterXml = ApprovalXmlWriter.prettyPrint(jacksonXml, 2);
+    Assertions.assertEquals(jacksonXml, approvalsWriterXml);
+  }
+  @Test
+  void xmlOutputByJacksonIsHandledCorrectlyWhenPrettyPrintingIsEnabled() throws JsonProcessingException
+  {
+    ObjectWriter objectWriter = new XmlMapper().configure(WRITE_XML_DECLARATION, true)
+        .writerWithDefaultPrettyPrinter();
+    String jacksonXml = objectWriter.writeValueAsString(new JacksonTestPOJO());
+    String approvalsWriterXml = ApprovalXmlWriter.prettyPrint(jacksonXml, 2);
+    Assertions.assertEquals(jacksonXml, approvalsWriterXml);
+  }
+  private static class JacksonTestPOJO
+  {
+    public String getSomeText()
+    {
+      return "Some text";
     }
-
-    @Test
-    public void xmlWithAttributes() {
-        Approvals.verifyXml("<xml b=\"123\" a=\"456\"><hello x=\"y\"/><start>hi</start></xml>");
+    public String getSomeTextWithAmpersand()
+    {
+      return "Some more text & some more";
     }
-
-    @Test
-    public void xmlWithDeepAttributes() {
-        Approvals
-                .verifyXml("<xml b=\"1\" a=\"1\"><branch1 b=\"1\" a=\"1\"/><branch2 b=\"1\" a=\"1\">hi</branch2></xml>");
+    public String getEmoji()
+    {
+      return "😸";
     }
-
-    @Test
-    public void xmlWithDeepAttributesWithScrubber() {
-        Approvals.verifyXml(
-                "<xml b=\"1\" a=\"1\"><branch1 b=\"1\" a=\"1\"/><branch2 b=\"1\" a=\"1\">hi</branch2></xml>",
-                new Options(new RegExScrubber("hi", "hello")));
-    }
-
-    @Test
-    public void invalidXml() {
-        Approvals.verifyXml("<xml><hello/><start>hi</xml>");
-        System.err.println("Note: The previous xml error (</start>) is expected ");
-    }
-
-    @Test
-    void xmlOutputByJacksonIsHandledCorrectly() throws JsonProcessingException {
-        ObjectWriter objectWriter = new XmlMapper()
-                .configure(WRITE_XML_DECLARATION, true)
-                .writer();
-
-        String jacksonXml = objectWriter.writeValueAsString(new JacksonTestPOJO());
-        String approvalsWriterXml = ApprovalXmlWriter.prettyPrint(jacksonXml, 2);
-
-        Assertions.assertEquals(jacksonXml, approvalsWriterXml);
-    }
-
-    @Test
-    void xmlOutputByJacksonIsHandledCorrectlyWhenPrettyPrintingIsEnabled() throws JsonProcessingException {
-        ObjectWriter objectWriter = new XmlMapper()
-                .configure(WRITE_XML_DECLARATION, true)
-                .writerWithDefaultPrettyPrinter();
-
-        String jacksonXml = objectWriter.writeValueAsString(new JacksonTestPOJO());
-        String approvalsWriterXml = ApprovalXmlWriter.prettyPrint(jacksonXml, 2);
-
-        Assertions.assertEquals(jacksonXml, approvalsWriterXml);
-    }
-
-    private static class JacksonTestPOJO {
-
-        public String getSomeText() {
-            return "Some text";
-        }
-
-        public String getSomeTextWithAmpersand() {
-            return "Some more text & some more";
-        }
-
-        public String getEmoji() {
-            return "😸";
-        }
-    }
+  }
 }
