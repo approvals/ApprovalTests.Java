d32887cefa31bd4b464fa25a0be77ffc8ae0e793
GitHub Action <action@github.com>
2024-09-12 20:57:34 +0000

    a   reformat code

--- approvaltests-util/src/main/java/com/spun/util/persistence/Saver2.java
+++ approvaltests-util/src/main/java/com/spun/util/persistence/Saver2.java
@@ -4,15 +4,17 @@ import com.spun.util.Tuple;
 import org.lambda.actions.Action2;
 import org.lambda.functions.Function2;
 
-public class Saver2 {
-    public static <In1, In2> Saver<Tuple<In1, In2>> create(Action2<In1, In2> lambda) {
-        return t -> {
-            lambda.call(t.getFirst(), t.getSecond());
-            return t;
-        };
-    }
-
-    public static <In1, In2> Saver<Tuple<In1, In2>> create(Function2<In1, In2, Tuple<In1, In2>> lambda) {
-        return t -> lambda.call(t.getFirst(), t.getSecond());
-    }
+public class Saver2
+{
+  public static <In1, In2> Saver<Tuple<In1, In2>> create(Action2<In1, In2> lambda)
+  {
+    return t -> {
+      lambda.call(t.getFirst(), t.getSecond());
+      return t;
+    };
+  }
+  public static <In1, In2> Saver<Tuple<In1, In2>> create(Function2<In1, In2, Tuple<In1, In2>> lambda)
+  {
+    return t -> lambda.call(t.getFirst(), t.getSecond());
+  }
 }
