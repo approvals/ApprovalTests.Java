1d441910d633a0e7614bc76c2ccca88e66b97723
Lars Eckart <lars.eckart@hey.com>
2024-09-16 19:35:42 +0300

    - r improve formatting

--- approvaltests/src/main/java/org/approvaltests/reporters/EnvironmentVariableReporter.java
+++ approvaltests/src/main/java/org/approvaltests/reporters/EnvironmentVariableReporter.java
@@ -8,32 +8,53 @@ import org.approvaltests.reporters.linux.MeldMergeReporter;
 import org.approvaltests.reporters.macosx.KaleidoscopeDiffReporter;
 import org.approvaltests.reporters.macosx.P4MergeReporter;
 import org.approvaltests.reporters.macosx.TkDiffReporter;
-import org.approvaltests.reporters.windows.*;
+import org.approvaltests.reporters.windows.AraxisMergeReporter;
+import org.approvaltests.reporters.windows.CodeCompareReporter;
+import org.approvaltests.reporters.windows.TortoiseDiffReporter;
+import org.approvaltests.reporters.windows.WinMergeReporter;
+import org.approvaltests.reporters.windows.WindowsDiffReporter;
 import org.lambda.functions.Function1;
 
-import java.util.*;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class EnvironmentVariableReporter implements ApprovalFailureReporter
 {
   private final ApprovalFailureReporter                                      reporter;
-  private static final Map<String, Class<? extends ApprovalFailureReporter>> REPORTER_MAP              = new MapBuilder(
-      "AraxisMergeReporter", AraxisMergeReporter.class).and("AutoApproveReporter", AutoApproveReporter.class)
-      .and("AutoApproveWhenEmptyReporter", AutoApproveWhenEmptyReporter.class)
-      .and("BeyondCompareReporter", BeyondCompareReporter.class).and("ClipboardReporter", ClipboardReporter.class)
-      .and("CodeCompareReporter", CodeCompareReporter.class)
-      .and("DelayedClipboardReporter", DelayedClipboardReporter.class)
-      .and("DiffMergeReporter", DiffMergeReporter.class).and("DiffReporter", DiffReporter.class)
-      .and("FileCaptureReporter", FileCaptureReporter.class).and("ImageReporter", ImageReporter.class)
-      .and("ImageWebReporter", ImageWebReporter.class).and("IntelliJReporter", IntelliJReporter.class)
-      .and("JunitReporter", JunitReporter.class).and("KDiff3Reporter", KDiff3Reporter.class)
-      .and("KaleidoscopeDiffReporter", KaleidoscopeDiffReporter.class)
-      .and("MeldMergeReporter", MeldMergeReporter.class).and("P4MergeReporter", P4MergeReporter.class)
-      .and("PitReporter", PitReporter.class).and("QuietReporter", QuietReporter.class)
-      .and("TestNgReporter", TestNgReporter.class).and("TextWebReporter", TextWebReporter.class)
-      .and("TkDiffReporter", TkDiffReporter.class).and("TortoiseDiffReporter", TortoiseDiffReporter.class)
-      .and("VisualStudioCodeReporter", VisualStudioCodeReporter.class)
-      .and("WinMergeReporter", WinMergeReporter.class).and("WindowsDiffReporter", WindowsDiffReporter.class);
+  // @formatter:off
+  private static final Map<String, Class<? extends ApprovalFailureReporter>> REPORTER_MAP =
+          new MapBuilder("AraxisMergeReporter", AraxisMergeReporter.class)
+          .and("AutoApproveReporter", AutoApproveReporter.class)
+          .and("AutoApproveWhenEmptyReporter", AutoApproveWhenEmptyReporter.class)
+          .and("BeyondCompareReporter", BeyondCompareReporter.class)
+          .and("ClipboardReporter", ClipboardReporter.class)
+          .and("CodeCompareReporter", CodeCompareReporter.class)
+          .and("DelayedClipboardReporter", DelayedClipboardReporter.class)
+          .and("DiffMergeReporter", DiffMergeReporter.class)
+          .and("DiffReporter", DiffReporter.class)
+          .and("FileCaptureReporter", FileCaptureReporter.class)
+          .and("ImageReporter", ImageReporter.class)
+          .and("ImageWebReporter", ImageWebReporter.class)
+          .and("IntelliJReporter", IntelliJReporter.class)
+          .and("JunitReporter", JunitReporter.class)
+          .and("KDiff3Reporter", KDiff3Reporter.class)
+          .and("KaleidoscopeDiffReporter", KaleidoscopeDiffReporter.class)
+          .and("MeldMergeReporter", MeldMergeReporter.class)
+          .and("P4MergeReporter", P4MergeReporter.class)
+          .and("PitReporter", PitReporter.class)
+          .and("QuietReporter", QuietReporter.class)
+          .and("TestNgReporter", TestNgReporter.class)
+          .and("TextWebReporter", TextWebReporter.class)
+          .and("TkDiffReporter", TkDiffReporter.class)
+          .and("TortoiseDiffReporter", TortoiseDiffReporter.class)
+          .and("VisualStudioCodeReporter", VisualStudioCodeReporter.class)
+          .and("WinMergeReporter", WinMergeReporter.class)
+          .and("WindowsDiffReporter", WindowsDiffReporter.class);
+    // @formatter:on
   public static final String                                                 ENVIRONMENT_VARIABLE_NAME = "APPROVAL_TESTS_USE_REPORTER";
   public static Function1<String, String>                                    ENVIRONMENT_VARIABLES     = System::getenv;
   public static final EnvironmentVariableReporter                            INSTANCE                  = new EnvironmentVariableReporter();
