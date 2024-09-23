04fe91a1dbe0999158974ed8188f61782d1080c2
Lars Eckart <lars.eckart@hey.com>
2024-09-23 19:56:41 +0300

    ! r can we use multiline strings

--- approvaltests/src/main/java/org/approvaltests/ApprovalsDuplicateVerifyException.java
+++ approvaltests/src/main/java/org/approvaltests/ApprovalsDuplicateVerifyException.java
@@ -4,11 +4,17 @@ public class ApprovalsDuplicateVerifyException extends RuntimeException
 {
   public ApprovalsDuplicateVerifyException(String file)
   {
-    super("Already approved: " + file + "\n"
-        + "By default, ApprovalTests only allows one verify() call per test.\n" + "To find out more, visit: \n"
-        + "https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/reference/Naming.md\n\n"
-        + "# Fixes\n" + "1. separate your test into two tests\n" + "2. add NamedParameters with the NamerFactory\n"
-        + "3. Override Approvals.settings() with either \n" + "\ta. allowMultipleVerifyCallsForThisClass\n"
-        + "\tb. allowMultipleVerifyCallsForThisMethod");
+    super("""
+        Already approved: %s
+        By default, ApprovalTests only allows one verify() call per test.
+        To find out more, visit:
+        https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/reference/Naming.md
+
+        # Fixes
+        1. separate your test into two tests
+        2. add NamedParameters with the NamerFactory
+        3. Override Approvals.settings() with either
+            a. allowMultipleVerifyCallsForThisClass
+            b. allowMultipleVerifyCallsForThisMethod""".formatted(file));;
   }
 }
