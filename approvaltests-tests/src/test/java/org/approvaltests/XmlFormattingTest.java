359e6aec8297c0a635793f260808084a94f1844c
Lars Eckart <lars.eckart@hey.com>
2024-09-30 20:52:25 +0300

    . t simplify test
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>
    Co-authored-by: Llewellyn Falco <llewellyn.falco@gmail.com>

--- approvaltests-tests/src/test/java/org/approvaltests/XmlFormattingTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/XmlFormattingTest.java
@@ -4,21 +4,9 @@ import org.approvaltests.core.Options;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-import javax.xml.transform.OutputKeys;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
-import java.io.Writer;
-
 class XmlFormattingTest
 {
-  @Disabled("SPIKE - continue next time")
+  @Disabled("SPIKE for #466 - continue next time")
   @Test
   void xmlWithEmojiesAndAmpersands()
   {
@@ -29,29 +17,7 @@ class XmlFormattingTest
           <emoji>😸</emoji>
         </a>
         """;
-    String input = expected.replaceAll("\n", "").replace("  ", "");
-    Approvals.verify(prettyPrint(input, 2), new Options().inline(expected));
-  }
-  private static String prettyPrint(String expected, int tabSize)
-  {
-    try
-    {
-      Source xmlInput = new StreamSource(new StringReader(expected));
-      StringWriter stringWriter = new StringWriter();
-      StreamResult xmlOutput = new StreamResult(stringWriter);
-      TransformerFactory transformerFactory = TransformerFactory.newInstance();
-      transformerFactory.setAttribute("indent-number", tabSize);
-      Transformer transformer = transformerFactory.newTransformer();
-      transformer.setOutputProperty(OutputKeys.INDENT, "yes");
-      transformer.transform(xmlInput, xmlOutput);
-      try (Writer writer = xmlOutput.getWriter())
-      {
-        return writer.toString();
-      }
-    }
-    catch (TransformerException | IOException e)
-    {
-      return expected;
-    }
+    String minimizedXml = expected.replaceAll("\n", "").replace("  ", "");
+    Approvals.verifyXml(minimizedXml, new Options().inline(expected));
   }
 }
