483cad5e002a07322bbf6a2b5cef0e7c45e6f6cc
GitHub Action <action@github.com>
2024-10-07 17:18:26 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/XmlFormattingTest.java
+++ approvaltests-tests/src/test/java/org/approvaltests/XmlFormattingTest.java
@@ -6,46 +6,46 @@ import org.junit.jupiter.api.Test;
 
 import java.io.*;
 
-class XmlFormattingTest {
-   // @Disabled("SPIKE for #466 - continue next time")
-    @Test
-    void xmlWithEmojiesAndAmpersands() {
-        var expected = """
-            <?xml version="1.0" encoding="UTF-8"?>
-            <a>
-              <b>😸 &amp; 🐶</b>
-            </a>
-            """;
-        String minimizedXml = expected.replaceAll("\n", " ").replace("  ", "");
-        verifyXml(minimizedXml, expected);
+class XmlFormattingTest
+{
+  // @Disabled("SPIKE for #466 - continue next time")
+  @Test
+  void xmlWithEmojiesAndAmpersands()
+  {
+    var expected = """
+        <?xml version="1.0" encoding="UTF-8"?>
+        <a>
+          <b>😸 &amp; 🐶</b>
+        </a>
+        """;
+    String minimizedXml = expected.replaceAll("\n", " ").replace("  ", "");
+    verifyXml(minimizedXml, expected);
+  }
+  private static void verifyXml(String minimizedXml, String expected)
+  {
+    Options options = new Options().inline(expected);
+    final String formattedXml = prettyPrintXml(minimizedXml, 2);
+    Approvals.verify(formattedXml, options.forFile().withExtension(".xml"));
+  }
+  private static String prettyPrintXml(String minimizedXml, int indent)
+  {
+    try
+    {
+      Builder builder = new Builder();
+      Document doc = builder.build(new StringReader(minimizedXml));
+      // create a string output stream
+      ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+      Serializer serializer = new Serializer(byteArrayOutputStream, "UTF-8");
+      serializer.setIndent(indent); // Increase indentation to make XML more readable
+      serializer.setLineSeparator("\n"); // Ensure new lines are added
+      serializer.setMaxLength(0); // Prevent line wrapping
+      serializer.write(doc);
+      return byteArrayOutputStream.toString("UTF-8");
     }
-
-    private static void verifyXml(String minimizedXml, String expected) {
-        Options options = new Options().inline(expected);
-        final String formattedXml = prettyPrintXml(minimizedXml, 2);
-        Approvals.verify(formattedXml, options.forFile().withExtension(".xml"));
+    catch (Exception e)
+    {
+      e.printStackTrace();
+      return minimizedXml + "\n\nXML Parsing Error:\n" + e.getMessage();
     }
-
-
-    private static String prettyPrintXml(String minimizedXml, int indent) {
-        try {
-            Builder builder = new Builder();
-            Document doc = builder.build(new StringReader(minimizedXml));
-
-            // create a string output stream
-            ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-            Serializer serializer = new Serializer(byteArrayOutputStream, "UTF-8");
-            serializer.setIndent(indent); // Increase indentation to make XML more readable
-            serializer.setLineSeparator("\n"); // Ensure new lines are added
-            serializer.setMaxLength(0); // Prevent line wrapping
-
-            serializer.write(doc);
-
-            return byteArrayOutputStream.toString("UTF-8");
-        } catch (Exception e) {
-            e.printStackTrace();
-            return minimizedXml + "\n\nXML Parsing Error:\n" + e.getMessage();
-        }
-    }
-
+  }
 }
