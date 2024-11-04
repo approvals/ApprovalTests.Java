045686f8155ba7ebb088d69ff61cffc2fa80c427
GitHub Action <action@github.com>
2024-11-04 17:41:08 +0000

    a   reformat code

--- approvaltests-tests/src/test/java/org/approvaltests/DeleteMeAfterExperiments.java
+++ approvaltests-tests/src/test/java/org/approvaltests/DeleteMeAfterExperiments.java
@@ -2,10 +2,11 @@ package org.approvaltests;
 
 import org.junit.jupiter.api.Test;
 
-class DeleteMeAfterExperiments {
-
-    @Test
-    void applesauce() {
-        Approvals.verify("test to see test report in github actions");
-    }
+class DeleteMeAfterExperiments
+{
+  @Test
+  void applesauce()
+  {
+    Approvals.verify("test to see test report in github actions");
+  }
 }
