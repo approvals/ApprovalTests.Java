554e5a37c489d23c730930b32f7c0da59d1e1059
Lars Eckart <lars.eckart@hey.com>
2024-11-04 19:40:22 +0200

    *** failing on purpose
    Co-Authored-By: Llewellyn Falco <llewellyn.falco@gmail.com>
    Co-Authored-By: asegrt <46486845+asegrt@users.noreply.github.com>

---
+++ approvaltests-tests/src/test/java/org/approvaltests/DeleteMeAfterExperiments.java
@@ -0,0 +1,11 @@
+package org.approvaltests;
+
+import org.junit.jupiter.api.Test;
+
+class DeleteMeAfterExperiments {
+
+    @Test
+    void applesauce() {
+        Approvals.verify("test to see test report in github actions");
+    }
+}
