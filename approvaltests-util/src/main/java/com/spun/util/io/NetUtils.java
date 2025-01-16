5e242fecb8fe938630f3703e5e9d6f65d838a798
Scott Wierschem <scottw@potentialpower.com>
2025-01-16 16:13:05 -0600

    - r Replaced URL with URI.create(url)

--- approvaltests-util/src/main/java/com/spun/util/io/NetUtils.java
+++ approvaltests-util/src/main/java/com/spun/util/io/NetUtils.java
@@ -6,6 +6,7 @@ import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.net.URI;
 import java.net.URL;
 
 /**
@@ -22,7 +23,7 @@ public class NetUtils
       {
         url += "?" + parameters;
       }
-      URL urlObj = new URL(url);
+      URL urlObj = URI.create(url).toURL();
       connection = (HttpURLConnection) urlObj.openConnection();
       connection.setRequestMethod("GET");
       connection.connect();
