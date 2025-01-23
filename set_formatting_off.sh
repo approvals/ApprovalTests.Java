f435eec84a99827e2f2f03635c94df13cb9453be
Scott Wierschem <scottw@potentialpower.com>
2025-01-23 16:44:19 -0600

    . e Testing Mac more

--- set_formatting_off.sh
+++ set_formatting_off.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
-sed -i 's/<!-- formatter_plugin_begin -->/<!-- formatter_plugin_begin/g' pom.xml
-sed -i 's/<!-- formatter_plugin_end -->/ formatter_plugin_end -->/g' pom.xml
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  backup = '.back'
+fi
+
+sed -i $backup 's/<!-- formatter_plugin_begin -->/<!-- formatter_plugin_begin/g' pom.xml
+sed -i $backup 's/<!-- formatter_plugin_end -->/ formatter_plugin_end -->/g' pom.xml
         
