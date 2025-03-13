9cc8bcd4ce17c9994a6a9b7be7c3d9e0d18892e8
Scott Wierschem <scottw@potentialpower.com>
2025-03-13 17:41:44 -0500

    . t Add logging to test

--- approvaltests/src/main/java/org/approvaltests/integrations/junit5/JupiterApprovals.java
+++ approvaltests/src/main/java/org/approvaltests/integrations/junit5/JupiterApprovals.java
@@ -1,9 +1,11 @@
 package org.approvaltests.integrations.junit5;
 
+import com.spun.util.logger.SimpleLogger;
 import org.approvaltests.SafetyCheckBeforeVerify;
 import org.approvaltests.Approvals;
 import org.approvaltests.core.Experimental;
 import org.approvaltests.core.Options;
+import org.junit.jupiter.api.DisplayNameGenerator;
 import org.junit.jupiter.api.DynamicTest;
 import org.lambda.actions.Action1;
 
@@ -20,9 +22,13 @@ public class JupiterApprovals
   }
   private static void checkOptionsWasUsed(Options actual, Options expected)
   {
-    if (!actual.forFile().getNamer().getAdditionalInformation()
-        .startsWith(expected.forFile().getNamer().getAdditionalInformation()))
+    String actualAdditionalInformation = actual.forFile().getNamer().getAdditionalInformation();
+    String expectedAdditionalInformation = expected.forFile().getNamer().getAdditionalInformation();
+    if (!actualAdditionalInformation
+        .startsWith(expectedAdditionalInformation))
     {
+      SimpleLogger.variable("Actual additional information", actualAdditionalInformation);
+      SimpleLogger.variable("Expected additional information", expectedAdditionalInformation);
       String helpMessage = "When using dynamic tests and Approvals, all calls to verify() must use the original Options or a derivative:  \n"
           + "   wrong: o -> Approvals.verify(result);  \n" + "   right: o -> Approvals.verify(result, o);  \n"
           + " More at: https://github.com/approvals/ApprovalTests.Java/blob/master/approvaltests/docs/how_to/UseTestFactory.md";
