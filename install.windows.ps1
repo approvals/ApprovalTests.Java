22f8eb50dc409e9ed9ef2d42243664ad44bae20f
Jay Bazuzi <jay@bazuzi.com>
2025-03-31 09:47:59 -0700

    . e update install to use openjdk and maven

--- install.windows.ps1
+++ install.windows.ps1
@@ -8,8 +8,8 @@
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/java-intellij.ps1 | iex
 
-#install project specific tools 
-choco install -y jdk8 
+#install project-specific tools
+choco install -y openjdk maven
 
 # Clone repo
 & "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/ApprovalTests.java.git C:\Code\ApprovalTests.java
