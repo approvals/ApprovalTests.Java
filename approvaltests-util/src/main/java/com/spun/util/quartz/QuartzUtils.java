86d7dc84251b25879009c0d8f2bbeaa696406fa9
Llewellyn Falco <llewellyn.falco@gmail.com>
2025-05-15 16:01:50 -0600

    ! r upgrade quartz
    Co-Authored-By: Jay Bazuzi <1259628+JayBazuzi@users.noreply.github.com>
    Co-Authored-By: Scott Wierschem <28421044+ScottBob@users.noreply.github.com>

--- approvaltests-util/src/main/java/com/spun/util/quartz/QuartzUtils.java
+++ approvaltests-util/src/main/java/com/spun/util/quartz/QuartzUtils.java
@@ -1,17 +1,21 @@
 package com.spun.util.quartz;
 
 import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
 
 import java.util.Date;
 
 public class QuartzUtils
 {
-  public static Trigger createTrigger(Trigger trigger, String name, String defaultGroup)
+  public static Trigger createTrigger(Trigger oldTrigger, String name, String defaultGroup)
   {
-    trigger.setName(name);
-    trigger.setGroup(defaultGroup);
-    trigger.setStartTime(new Date());
-    trigger.setEndTime(new Date(Long.MAX_VALUE));
-    return trigger;
+    // Get a TriggerBuilder that is configured to produce a Trigger
+    // identical to the oldTrigger one in terms of job, schedule type etc.
+    TriggerBuilder<? extends Trigger> builder = oldTrigger.getTriggerBuilder();
+    // Now, set the new identity (name and group) and start/end times
+    Trigger newTrigger = builder.withIdentity(name, defaultGroup).startAt(new Date()) // Start immediately
+        .endAt(new Date(Long.MAX_VALUE)) // Effectively never ends
+        .build();
+    return newTrigger;
   }
 }
