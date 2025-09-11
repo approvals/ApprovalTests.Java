0fb1d704473f862e3cf4181838a26fdf8936448a
Scott Wierschem <scottw@potentialpower.com>
2025-09-11 15:54:57 -0500

    . t Remove log messages

--- approvaltests-util-tests/src/test/java/com/spun/util/DatabaseUtilsTest.java
+++ approvaltests-util-tests/src/test/java/com/spun/util/DatabaseUtilsTest.java
@@ -1,5 +1,6 @@
 package com.spun.util;
 
+import com.spun.util.logger.SimpleLogger;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,19 +8,22 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class DatabaseUtilsTest
 {
   @Test
-  public void testTransaction()
+  public void testTransaction() throws Exception
   {
-    MockConnection connection1 = new MockConnection();
-    MockConnection connection2 = new MockConnection();
-    DatabaseUtils.beginTransaction(connection1);
-    assertEquals(1, connection1.autoCommitCount);
-    DatabaseUtils.beginTransaction(connection1);
-    assertEquals(1, connection1.autoCommitCount);
-    connection1 = null;
-    System.gc();
-    DatabaseUtils.beginTransaction(connection2);
-    assertEquals(1, connection2.autoCommitCount);
-    DatabaseUtils.commit(connection2);
-    assertEquals(1, connection2.commitCount);
+    try (AutoCloseable quiet = SimpleLogger.quiet())
+    {
+      MockConnection connection1 = new MockConnection();
+      MockConnection connection2 = new MockConnection();
+      DatabaseUtils.beginTransaction(connection1);
+      assertEquals(1, connection1.autoCommitCount);
+      DatabaseUtils.beginTransaction(connection1);
+      assertEquals(1, connection1.autoCommitCount);
+      connection1 = null; // So that GC can run
+      System.gc();
+      DatabaseUtils.beginTransaction(connection2);
+      assertEquals(1, connection2.autoCommitCount);
+      DatabaseUtils.commit(connection2);
+      assertEquals(1, connection2.commitCount);
+    }
   }
 }
