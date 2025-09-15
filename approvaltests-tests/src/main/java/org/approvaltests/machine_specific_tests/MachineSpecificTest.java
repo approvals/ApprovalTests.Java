9fcd64cba515fae47ae968478ce3ee3935606d63
Jay Bazuzi <jay@bazuzi.com>
2025-09-15 11:06:15 -0700

    ! F include test name in MachineSpecificTest message

--- approvaltests-tests/src/main/java/org/approvaltests/machine_specific_tests/MachineSpecificTest.java
+++ approvaltests-tests/src/main/java/org/approvaltests/machine_specific_tests/MachineSpecificTest.java
@@ -2,7 +2,9 @@ package org.approvaltests.machine_specific_tests;
 
 import com.spun.util.SystemUtils;
 import com.spun.util.logger.SimpleLogger;
+import org.approvaltests.Approvals;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestInfo;
 
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
@@ -10,24 +12,28 @@ public class MachineSpecificTest
 {
   private static boolean DISPLAYED = false;
   @BeforeEach
-  public void beforeMethod()
+  public void beforeMethod(TestInfo testInfo)
   {
     if (!MachineSpecific.isMachineConfiguredForTesting())
     {
-      displayMessage();
+      displayMessage(testInfo);
       assumeTrue(false);
     }
   }
-  private void displayMessage()
+  private void displayMessage(TestInfo testInfo)
   {
     if (!DISPLAYED)
     {
       DISPLAYED = true;
-      String message = String.format(
-          "This machine isn't configured to run machine_specific_tests.\n" + "To run these either\n"
-              + "  1) Set machine_specific_tests.MachineSpecific.FORCE_RUN=true\n"
-              + "  2) Add \"%s\" to machine_specific_tests.MachineSpecific.MACHINES",
-          SystemUtils.getComputerName());
+      String testName = testInfo.getTestClass().get().getName();
+      String message = String.format("" //
+          + "This machine isn't configured to run MachineSpecificTest.\n" //
+          + "\n" //
+          + "To run these either\n" //
+          + "  1) Set machine_specific_tests.MachineSpecific.FORCE_RUN=true\n" //
+          + "  2) Add \"%s\" to machine_specific_tests.MachineSpecific.MACHINES\n" //
+          + "\n" //
+          + "Triggered by %s", SystemUtils.getComputerName(), testName);
       SimpleLogger.warning(message);
     }
   }
