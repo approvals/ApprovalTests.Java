881b89db8aaae96023c95f7b83cbc64c7c9d4c31
Lars Eckart <lars.eckart@hey.com>
2025-10-13 19:35:26 +0300

    - r delete hadoop module
    was already not in the published jars anymore but keeps getting flagged by snyk and others.
    
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>
    Co-authored-by: Llewellyn Falco <llewellyn.falco@gmail.com>

--- approvaltests-hadoop/src/main/java/org/approvaltests/hadoop/Transform.java
+++
@@ -1,9 +0,0 @@
-package org.approvaltests.hadoop;
-
-public interface Transform<KeyIn, ValueIn, KeyOut, ValueOut>
-{
-  public Class<KeyIn> getKeyInType();
-  public Class<ValueIn> getValueInType();
-  public Class<KeyOut> getKeyOutType();
-  public Class<ValueOut> getValueOutType();
-}
