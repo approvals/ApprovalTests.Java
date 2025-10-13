75febf64ac8b62e5e4b5efad9e6ca775b10cf3a9
Lars Eckart <lars.eckart@hey.com>
2025-10-13 19:42:54 +0300

    - r remove old release scripts
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>
    Co-authored-by: Llewellyn Falco <llewellyn.falco@gmail.com>

--- build/deploy_major_release.py
+++
@@ -1,7 +0,0 @@
-#! /usr/bin/env python3
-import scripts_java.include_approvals_path # Note This is used!
-
-from scripts_java.prepare_release import build
-
-if __name__ == '__main__':
-    build(lambda v: v.update_major())
--- build/scripts_java/release_constants.py
+++
@@ -1,8 +0,0 @@
-class ReleaseConstants:
-    def __init__(self) -> None:
-        self.approval_tests_dir = F"../ApprovalTests.java"
-        self.build_dir = F"./build"
-        self.starter_project_dir = F"../ApprovalTests.java.StarterProject"
-
-
-release_constants = ReleaseConstants()
