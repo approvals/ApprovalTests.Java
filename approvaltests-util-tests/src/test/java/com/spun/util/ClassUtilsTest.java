dcf5adf72e6f4909f7f25749275369eb400d307f
Lars Eckart <lars.eckart@hey.com>
2026-01-05 20:35:02 +0200

    - t refactor tests
    Co-authored-by: Jay Bazuzi <jay@bazuzi.com>
    Co-authored-by: Llewellyn Falco <llewellyn.falco@gmail.com>

--- approvaltests-util-tests/src/test/java/com/spun/util/ClassUtilsTest.java
+++ approvaltests-util-tests/src/test/java/com/spun/util/ClassUtilsTest.java
@@ -1,12 +1,12 @@
 package com.spun.util;
 
-import org.junit.jupiter.api.Test;
-
 import java.io.File;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
@@ -46,50 +46,41 @@ public class ClassUtilsTest
   @Test
   void testFindWithFullPackagePath()
   {
-    // Standard case: full package path exists in directory structure
-    // Get the source directory for this test class as the base
-    File sourceDir = ClassUtils.getSourceDirectory(getClass());
-    // Go up to 'java' directory (from com/spun/util -> src/test/java)
-    File baseDir = sourceDir.getParentFile().getParentFile().getParentFile();
-    List<String> matches = Arrays.asList("com", "spun", "util", "ClassUtilsTest.java");
-    File found = ClassUtils.find(baseDir, matches);
-    assertNotNull(found, "Should find file with full package path");
-    assertEquals("ClassUtilsTest.java", found.getName());
+    assertFileFound("com", "spun", "util", "ClassUtilsTest.java");
   }
 
   @Test
   void testFindWithPartialPackagePath()
   {
     // Kotlin-style case: common root package omitted from directory structure
-    // File is at: src/test/java/com/spun/util/ClassUtilsTest.java
-    // But we search as if package was "fake.root.com.spun.util"
-    File sourceDir = ClassUtils.getSourceDirectory(getClass());
-    File baseDir = sourceDir.getParentFile().getParentFile().getParentFile();
-    List<String> matches = Arrays.asList("fake", "root", "com", "spun", "util", "ClassUtilsTest.java");
-    File found = ClassUtils.find(baseDir, matches);
-    assertNotNull(found, "Should find file even when leading package segments don't exist in directory");
-    assertEquals("ClassUtilsTest.java", found.getName());
+    assertFileFound("fake", "root", "com", "spun", "util", "ClassUtilsTest.java");
   }
 
   @Test
   void testFindWithOnlyFilename()
   {
-    // Edge case: only filename, no package segments
-    File sourceDir = ClassUtils.getSourceDirectory(getClass());
-    File baseDir = sourceDir.getParentFile().getParentFile().getParentFile();
-    List<String> matches = Arrays.asList("ClassUtilsTest.java");
-    File found = ClassUtils.find(baseDir, matches);
-    assertNotNull(found, "Should find file with just filename");
-    assertEquals("ClassUtilsTest.java", found.getName());
+    // Edge case: only filename, no package segments (default package)
+    assertFileFound("ClassUtilsTest.java");
+  }
+
+  private void assertFileFound(String... pathSegments)
+  {
+    File found = findInSourceRoot(pathSegments);
+    File sourceFile = ClassUtils.getAdjacentFile(this.getClass(), "ClassUtilsTest.java");
+    assertEquals(found.getAbsolutePath(), sourceFile.getAbsolutePath());
   }
 
   @Test
   void testFindNonExistentFile()
   {
-    File sourceDir = ClassUtils.getSourceDirectory(getClass());
-    File baseDir = sourceDir.getParentFile().getParentFile().getParentFile();
-    List<String> matches = Arrays.asList("com", "spun", "util", "NonExistentFile.java");
-    File found = ClassUtils.find(baseDir, matches);
+    File found = findInSourceRoot("com", "spun", "util", "NonExistentFile.java");
     assertNull(found, "Should return null for non-existent file");
+  }
+
+  private File findInSourceRoot(String... pathSegments)
+  {
+    File sourceDir = ClassUtils.getSourceDirectory(getClass());
+    File sourceRoot = sourceDir.getParentFile().getParentFile().getParentFile();
+    return ClassUtils.find(sourceRoot, Arrays.asList(pathSegments));
   }
 }
